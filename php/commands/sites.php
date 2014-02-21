ffe278637f4e7a16ef5f969c399333da2fd1becc
Ben Sheldon <bensheldon@gmail.com>
2014-02-20 18:27:37 -0800

    tableize sites command

--- php/commands/sites.php
+++ php/commands/sites.php
@@ -8,11 +8,26 @@ class Sites_Command extends Terminus_Command {
 	 * Show a list of your sites on Pantheon
 	 */
 	public function show() {
-    $sites = $this->terminus_request('user', $this->session->user_uuid, 'sites');
-    var_dump($sites);
+    $sites = $this->terminus_request('user', $this->session->user_uuid, 'sites', 'GET', Array('hydrated' => true))['data'];
+
+    print_r($sites);
+    $headers = Array('Site', 'Framework', 'Service Level', 'UUID');
+    $rows = Array();
+
+    foreach($sites as $id => $site) {
+      $rows[] = Array(
+        $site->information->name,
+        $site->information->framework,
+        $site->information->service_level,
+        $id
+      );
+    }
+
+    $table = new \cli\Table();
+    $table->setHeaders($headers);
+    $table->setRows($rows);
+    $table->display();
 	}
-
-
 }
 
 Terminus::add_command( 'sites', 'Sites_Command' );
