cd6f8b6b553e70ff6320a3a0a35e7a081e621316
Josh Koenig <josh@getpantheon.com>
2014-02-21 17:18:46 -0800

    adding local cachability and nocache pattern

--- php/commands/sites.php
+++ php/commands/sites.php
@@ -6,13 +6,16 @@
 class Sites_Command extends Terminus_Command {
 	/**
 	 * Show a list of your sites on Pantheon
+	 *
+   *  ## OPTIONS
+   *
+	 * [--nocache]
+	 * : Get a fresh list of sites from the server side.
 	 */
-	public function show( $arguments ) {
-    $sites = $this->terminus_request('user', $this->session->user_uuid, 'sites', 'GET', Array('hydrated' => true))['data'];
-
+	public function show( $args, $assoc_args ) {
+    $sites = $this->fetch_sites(isset($assoc_args['nocache']));
     $headers = Array('Site', 'Framework', 'Service Level', 'UUID');
     $rows = Array();
-    var_dump($arguments);
     foreach($sites as $id => $site) {
       $rows[] = Array(
         $site->information->name,
@@ -26,6 +29,25 @@ class Sites_Command extends Terminus_Command {
     $table->setHeaders($headers);
     $table->setRows($rows);
     $table->display();
+	}
+
+	/**
+	 * Helper code to grab sites and manage local cache.
+	 */
+	private function fetch_sites( $nocache = false ) {
+	  $sites = $this->cache->get_data('sites');
+	  if (!$sites || $nocache) {
+	    Terminus::log( 'Fetching site list from Pantheon' );
+      $request = $this->terminus_request( 'user',
+                                        $this->session->user_uuid,
+                                        'sites',
+                                        'GET',
+                                        Array('hydrated' => true));
+      # TODO: handle errors well.
+      $sites = $request['data'];
+      $this->cache->put_data( 'sites', $sites );
+    }
+    return $sites;
 	}
 }
 
