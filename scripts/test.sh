93b674ea7d7f612013ee077fcba935a71925098f
Wink <wink@getpantheon.com>
2014-09-25 18:15:48 +0000

    Quicker lint test

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,8 +3,9 @@
 set -ex
 
 # Basic lint test
-find . -type f -name "*.php" -exec php -l {} \;
-
+#find . -type f -name "*.php" -exec php -l {} \;
+echo $TRAVIS_COMMIT
+for file in $( git diff --name-only $TRAVIS_COMMIT | grep ".php" ); do php -l $file; done
 # Run the unit tests
 # phpunit
 
