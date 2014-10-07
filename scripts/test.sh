dd400bd275b851f26a0edd02ac73829447c7f855
Wink <wink@getpantheon.com>
2014-10-07 21:09:50 +0000

    Fix build script

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,11 @@
 set -ex
 
 # Basic lint test
-for f in $( git diff-tree $TRAVIS_COMMIT --name-only -r ) ; do php -l ; done
+for f in $( git diff-tree $TRAVIS_COMMIT --name-only -r ) ; do php -l $f ; done
 
 # Run the unit tests
 # phpunit
+phpunit
 
 # Run the functional tests
 php behat.phar --format pretty
-
-
