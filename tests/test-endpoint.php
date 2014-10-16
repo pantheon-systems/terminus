85a5a7e3d0e5f656d88ca7dc523fbf354b855a34
Michael Van Winkle <mike@mikevanwinkle.com>
2014-10-16 09:29:45 -0700

    Updated test for Endpoint class

--- tests/test-endpoint.php
+++ tests/test-endpoint.php
@@ -7,8 +7,19 @@ use \Terminus\Endpoint;
  class EndpointTest extends PHPUnit_Framework_TestCase {
 
    function testEndpoints() {
-     if( "dashboard.getpantheon.com" === TERMINUS_HOST ) {
-       $this->assertEquals( 'https://dashboard.getpantheon.com/api/users/UUID/sites', Endpoint::get( array('realm'=>'user','path' => 'sites', 'uuid'=> 'UUID') ) );
+     if( \Terminus\Utils\is_hermes() ) {
+       // expected => test
+       $tests = array(
+         'https://dashboard.getpantheon.com/api/users/UUID/sites' => array(
+           'realm'=>'user','path' => 'sites', 'uuid'=> 'UUID'
+         ),
+         'https://dashboard.getpantheon.com/api/products' => array(
+           'realm'=>'products','path' => false, 'uuid'=> 'public'
+         ),
+       );
+       foreach( $tests as $expected => $args) {
+         $this->assertEquals( $expected, Endpoint::get( $args ) );
+       }
      } else {
        $this->assertEquals( 'https://terminus.getpantheon.com/terminus.php?user=UUID&path=sites', Endpoint::get( array('realm'=>'user','path' => 'sites', 'uuid'=> 'UUID') ) );
      }
