4cba92ca5b03b167844773e1f487e7318abb504e
mikevanwinkle <mike@mikevanwinkle.com>
2014-10-17 21:47:55 +0000

    send body

--- php/Terminus/Request.php
+++ php/Terminus/Request.php
@@ -21,8 +21,16 @@ class Request {
   public static function send( $url, $method, $data = array() ) {
     // create a new Guzzle\Http\Client
     $browser = new Browser;
-    $allow_redirects = @$data['allow_redirects'] ?: false;
-    $request = $browser->createRequest($method, $url, null, null, array('allow_redirects' => $allow_redirects ) );
+    $options = array();
+    $options['allow_redirects'] = @$data['allow_redirects'] ?: false;
+    $options['json'] = @$data['json'] ?: false;
+    if( @$data['body'] ) {
+      $options['body'] = $data['body'];
+    }
+    $options['verify'] = false;
+
+    $request = $browser->createRequest($method, $url, null, null, $options );
+
     if( !empty($data['postdata']) ) {
       foreach( $data['postdata'] as $k=>$v ) {
         $request->setPostField($k,$v);
@@ -41,6 +49,13 @@ class Request {
       }
     }
 
+    if ( getenv("debug") === 1 ) {
+      print "####### DEBUG #######".PHP_EOL;
+      print $request->getRawHeaders();
+      print_r($data);
+      print "####### END DEBUG #####".PHP_EOL.PHP_EOL;
+    }
+
     if ( getenv("BUILD_FIXTURES") ) {
       Fixtures::put("request_headers", $request->getRawHeaders());
     }
@@ -48,7 +63,7 @@ class Request {
     $response = $request->send();
 
     if ( getenv("BUILD_FIXTURES") ) {
-      Fixtures::put("response", json_encode( $response, 1) );
+      Fixtures::put("response", serialize( $response ) );
     }
 
     return $response;
