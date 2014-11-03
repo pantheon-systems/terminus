0d8fb453d1c1f945d5bb6332b27467276d1a71e8
Michael Van Winkle <mike@mikevanwinkle.com>
2014-11-03 08:32:42 -0800

    More test

--- tests/test-utils.php
+++ tests/test-utils.php
@@ -30,4 +30,16 @@
      $this->assertTrue( \Terminus\Utils\result_is_multiobj( $arraymultiobj ));
    }
 
+  function testFilenameFromUrl() {
+    $url = 'https://pantheon-backups.s3.amazonaws.com/aaa313ea-d667-4cf6-b165-31a4a03abbc0/dev/1411761319_export/miketestsite_dev_2014-09-26T19-55-19_UTC_database.sql.gz?Signature=dK%2FOf7EtMwbjCpmnuBJ8S8ApezE%3D&Expires=1414793205&AWSAccessKeyId=AKIAJEYKXMCPBZQYJYXQ';
+    $filename = \Terminus\Utils\get_filename_from_url($url);
+    $this->assertEquals("miketestsite_dev_2014-09-26T19-55-19_UTC_database.sql.gz",$filename);
+  }
+
+  function testSqlFromZip() {
+    $target = '/tmp/miketestsite_dev_2014-10-30T18-59-07_UTC_database.sql.gz';
+    $actual = Terminus\Utils\sql_from_zip($target);
+    $this->assertEquals('/tmp/miketestsite_dev_2014-10-30T18-59-07_UTC_database.sql', $actual);
+  }
+
  }
