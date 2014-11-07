ecf190b51b97c5849f5e31f0de8a3d01a0b9e1cc
Michael Van Winkle <mike@mikevanwinkle.com>
2014-11-07 14:24:39 -0800

    turning off build fixtures

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@
 define('CLI_ROOT', dirname(__DIR__) );
 define('TERMINUS_CMD','php '.CLI_ROOT.'/php/boot-fs.php');
 putenv("CLI_TEST_MODE=1");
-putenv("BUILD_FIXTURES=1");
+//putenv("BUILD_FIXTURES=1");
 require_once CLI_ROOT.'/php/boot-fs.php';
 use Terminus\Fixtures;
 use Terminus\Session;
