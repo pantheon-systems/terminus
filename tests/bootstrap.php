e01d9ccfa7a555e053f37142d93728a41be05748
Mike Van Winkle <mike@mikevanwinkle.com>
2015-01-13 22:10:47 +0000

    Fix unit tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,10 @@
  */
 define('CLI_ROOT', dirname(__DIR__) );
 define('TERMINUS_CMD','php '.CLI_ROOT.'/php/boot-fs.php');
-putenv("CLI_TEST_MODE=1");
-putenv("BUILD_FIXTURES=0");
-putenv("USE_FIXTURES=1");
+putenv('CLI_TEST_MODE=1');
+putenv('BUILD_FIXTURES=0');
+putenv('USE_FIXTURES=1');
+putenv('debug=1');
 require_once CLI_ROOT.'/php/boot-fs.php';
 use Terminus\Fixtures;
 use Terminus\Session;
