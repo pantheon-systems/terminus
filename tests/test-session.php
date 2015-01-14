69f05fd51f7e42fb98ed3b40cc771461ac1805a0
Mike Van Winkle <mike@mikevanwinkle.com>
2015-01-14 20:18:40 +0000

    Expires nexxt year

--- tests/test-session.php
+++ tests/test-session.php
@@ -12,7 +12,7 @@ use Terminus\Session;
      $data = new stdClass();
      $data->session = 'dca4f8cd-9ec2-4117-957f-fc5230c23737:7c468270-66e9-11e4-aa9b-bc764e111d20:eEFVhz2Dc87oZIJxqBtaM';
      $data->user_uuid = 'dca4f8cd-9ec2-4117-957f-fc5230c23737';
-     $data->session_expires_time = 1417830572;
+     $data->session_expires_time = 1421186762;
      $data->email = "mike+test@mikevanwinkle.com";
 
      Session::instance()->setData($data);
