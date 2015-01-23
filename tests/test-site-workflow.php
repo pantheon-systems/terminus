cc39dbfc8079def58a687e7b5d946cfb7fc0ee2f
Mike Van Winkle <mike@mikevanwinkle.com>
2015-01-23 23:15:16 +0000

    adding site workflow test

---
+++ tests/test-site-workflow.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Testing class for \Terminus\Utils
+ *
+ */
+use \Terminus\Helpers\Input;
+use \Terminus\SiteWorkflow;
+use \Terminus\SiteFactory;
+
+ class SiteWorkFlowTest extends PHPUnit_Framework_TestCase {
+
+   function testSiteWorkflowCreate() {
+     $site = SiteFactory::instance('phpunittest');
+
+     $workflow = SiteWorkflow::createWorkflow('update_site_organization_membership', $site);
+
+     $this->assertInstanceOf('Terminus\SiteWorkflow',$workflow);
+     $this->assertInstanceOf('Terminus\Site',$workflow->site);
+     $this->assertNull($workflow->status());
+     $workflow->start();
+   }
+
+ }
