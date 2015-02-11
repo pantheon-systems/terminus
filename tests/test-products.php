fa6898b11d54ed5f0e62cfda819a9fa0975205de
Mike Van Winkle <mike@mikevanwinkle.com>
2015-02-11 20:02:26 +0000

    Refactor testing to use php-vcr instead of home-brewed fixtures.

--- tests/test-products.php
+++ tests/test-products.php
@@ -3,6 +3,12 @@ use \Terminus\Products;
 
 class ProductsTest extends PHPUnit_Framework_TestCase {
 
+  /**
+   * Test product functions
+   *
+   * @vcr productsbyid
+   *
+   */
  function testProductsInstance() {
     $products = Products::instance();
     $test = $products->getById('3b754bc2-48f8-4388-b5b5-2631098d03de');
