3761345d0b7c7a46857ecf954068176aab40dfe0
Elliot Voris <elliot@voris.me>
2015-02-11 20:34:50 -0600

    That space shouldn't be there. My bad.
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- php/commands/site.php
+++ php/commands/site.php
@@ -261,7 +261,7 @@ class Site_Command extends Terminus_Command {
     }
     $site = SiteFactory::instance(Input::site($assoc_args));
     $env = Input::optional( 'env', $assoc_args );
-    $env = $env ? sprintf ( "#%s", $env ) : null;
+    $env = $env ? sprintf( "#%s", $env ) : null;
     $url = sprintf("https://dashboard.getpantheon.com/sites/%s%s", $site->getId(), $env);
     if ( isset($assoc_args['print']) ) {
       Logger::coloredOutput("%GDashboard URL:%n " . $url);
