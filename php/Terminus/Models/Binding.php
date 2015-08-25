9956f0bf5f29127baa7b8763b82749115048b448
Sara McCutcheon <tesladethray@Tima.local>
2015-08-25 11:58:27 -0700

    Binding & Bindings now children of abstracts TerminusModel & TerminusCollection

--- php/Terminus/Models/Binding.php
+++ php/Terminus/Models/Binding.php
@@ -1,32 +1,6 @@
 <?php
+
 namespace Terminus\Models;
-use \Terminus\Request;
 
-class Binding {
-  public $id;
-  public $attributes;
-  public $collection;
-  public $environment;
-
-  public function __construct($attributes, $options = array()) {
-    $this->id = $attributes->id;
-    $this->attributes = $attributes;
-    $this->collection = $options['collection'];
-    $this->environment = $options['environment'];
-
-    return $this;
-  }
-
-  /**
-   * Returns given attribute, if present
-   *
-   * @param [string] $attribute Name of attribute requested
-   * @return [mixed] $this->attributes->$attributes;
-   */
-  public function get($attribute) {
-    if(isset($this->attributes->$attribute)) {
-      return $this->attributes->$attribute;
-    }
-    return null;
-  }
+class Binding extends TerminusModel {
 }
--- php/Terminus/SiteOrganizationMembership.php
+++ php/Terminus/SiteOrganizationMembership.php
@@ -3,7 +3,7 @@
 namespace Terminus;
 use \ReflectionClass;
 use \Terminus\Request;
-use \Terminus\Collections\Bindings;
+use \Terminus\Models\Collections\Bindings;
 
 class SiteOrganizationMembership {
   private $id;
