1ea3f2e7db04ac5da6252f6d6114d4c0c57c25b0
Sara McCutcheon <tesladethray@gmail.com>
2015-09-11 14:19:07 -0700

    Switched Terminus logger for katzgrau/KLogger

--- php/Terminus/Dispatcher/RootCommand.php
+++ php/Terminus/Dispatcher/RootCommand.php
@@ -11,49 +11,48 @@ class RootCommand extends CompositeCommand {
 
   function __construct() {
     $this->parent = false;
-
     $this->name = 'terminus';
-
     $this->shortdesc = 'Manage Pantheon through the command-line.';
   }
 
   function get_longdesc() {
     $binding = array();
 
-    foreach ( \Terminus::get_configurator()->get_spec() as $key => $details ) {
-      if ( false === $details['runtime'] )
+    foreach (\Terminus::get_configurator()->get_spec() as $key => $details) {
+      if (
+        ($details['runtime'] === false)
+        || isset($details['deprecated'])
+        || (isset($details['hidden']))
+      ) {
         continue;
-
-      if ( isset( $details['deprecated'] ) )
-        continue;
-
-      if ( isset( $details['hidden'] ) )
-        continue;
-
-      if ( true === $details['runtime'] )
-        $synopsis = "--[no-]$key";
-      else
+      } if ($details['runtime']) {
+        $synopsis = "--[no-]$key"; 
+      } else {
         $synopsis = "--$key" . $details['runtime'];
+      }
 
       $binding['parameters'][] = array(
         'synopsis' => $synopsis,
-        'desc' => $details['desc']
-      );
+        'desc'     => $details['desc']
+     );
     }
 
-    return Utils\mustache_render( 'man-params.mustache', $binding );
+    if ((boolean)\Terminus::get_config('json')) {
+      return $binding;
+    }
+    return Utils\mustache_render('man-params.mustache', $binding);
   }
 
-  function find_subcommand( &$args ) {
-    $command = array_shift( $args );
+  function find_subcommand(&$args) {
+    $command = array_shift($args);
 
-    Utils\load_command( $command );
+    Utils\load_command($command);
 
-    if ( !isset( $this->subcommands[ $command ] ) ) {
+    if (!isset($this->subcommands[$command])) {
       return false;
     }
 
-    return $this->subcommands[ $command ];
+    return $this->subcommands[$command];
   }
 
   function get_subcommands() {
--- php/Terminus/Runner.php
+++ php/Terminus/Runner.php
@@ -5,74 +5,80 @@ namespace Terminus;
 use Terminus;
 use Terminus\Utils;
 use Terminus\Dispatcher;
+use Terminus\KLogger;
 
 class Runner {
+  public $config;
+  public $extra_config;
 
-  private $global_config_path, $project_config_path;
-
-  public $config, $extra_config;
-
-  private $arguments, $assoc_args;
-
+  private $arguments;
+  private $assoc_args;
   private $_early_invoke = array();
+  private $global_config_path;
+  private $project_config_path;
 
-  public function __get( $key ) {
-    if ( '_' === $key[0] )
+  public function __construct() {
+    $this->init_config();
+    $this->init_colorization();
+    $this->init_logger();
+    $this->init_outputter();
+  }
+
+  public function __get($key) {
+    if ('_' === $key[0])
       return null;
 
     return $this->$key;
   }
 
-  public function find_command_to_run( $args ) {
+  public function find_command_to_run($args) {
     $command = \Terminus::get_root_command();
 
     $cmd_path = array();
-
-
-    while ( !empty( $args ) && $command->can_have_subcommands() ) {
+    while (!empty($args) && $command->can_have_subcommands()) {
       $cmd_path[] = $args[0];
-      $full_name = implode( ' ', $cmd_path );
+      $full_name  = implode(' ', $cmd_path);
 
-      $subcommand = $command->find_subcommand( $args );
+      $subcommand = $command->find_subcommand($args);
 
-      if ( !$subcommand ) {
+      if (!$subcommand) {
         return sprintf(
           "'%s' is not a registered command. See 'terminus help'.",
           $full_name
-        );
+       );
       }
 
       $command = $subcommand;
     }
 
-    return array( $command, $args, $cmd_path );
+    return array($command, $args, $cmd_path);
   }
 
-  public function run_command( $args, $assoc_args = array() ) {
-    $r = $this->find_command_to_run( $args );
-    if ( is_string( $r ) ) {
-      Terminus::error( $r );
+  public function run_command($args, $assoc_args = array()) {
+    $r = $this->find_command_to_run($args);
+    if (is_string($r)) {
+      Terminus::error($r);
     }
 
-    list( $command, $final_args, $cmd_path ) = $r;
+    list($command, $final_args, $cmd_path) = $r;
 
-    $name = implode( ' ', $cmd_path );
+    $name = implode(' ', $cmd_path);
 
-    if ( isset( $this->extra_config[ $name ] ) ) {
+    if (isset($this->extra_config[ $name ])) {
       $extra_args = $this->extra_config[ $name ];
     } else {
       $extra_args = array();
     }
 
     try {
-      $command->invoke( $final_args, $assoc_args, $extra_args );
-    } catch ( Terminus\Iterators\Exception $e ) {
-      Terminus::error( $e->getMessage() );
+      $command->invoke($final_args, $assoc_args, $extra_args);
+    } catch (Terminus\Iterators\Exception $e) {
+      Terminus::error($e->getMessage());
     }
   }
 
   private function _run_command() {
-    $this->run_command( $this->arguments, $this->assoc_args );
+    $this->run_command($this->arguments, $this->assoc_args);
   }
 
   public function in_color() {
@@ -80,7 +86,7 @@ class Runner {
   }
 
   private function init_colorization() {
-    if ( 'auto' === $this->config['colorize'] ) {
+    if ($this->config['colorize'] == 'auto') {
       $this->colorize = !\cli\Shell::isPiped();
     } else {
       $this->colorize = $this->config['colorize'];
@@ -88,12 +94,17 @@ class Runner {
   }
 
   private function init_logger() {
-    if ( $this->config['silent'] )
-      $logger = new \Terminus\Loggers\Quiet;
-    else
-      $logger = new \Terminus\Loggers\Regular( $this->in_color() );
+    $logger = new KLogger(array('config' => $this->config));
+    Terminus::set_logger($logger);
+  }
 
-    Terminus::set_logger( $logger );
+  private function init_outputter() {
+    if ($this->config['silent']) {
+      $outputter = new \Terminus\Loggers\Quiet;
+    } else {
+      $outputter = new \Terminus\Loggers\Regular($this->in_color());
+    }
+    Terminus::set_outputter($outputter);
   }
 
   private function init_config() {
@@ -101,59 +112,54 @@ class Runner {
 
     // Runtime config and args
     {
-      list( $args, $assoc_args, $runtime_config ) = $configurator->parse_args(
-        array_slice( $GLOBALS['argv'], 1 ) );
+      list($args, $assoc_args, $runtime_config) = $configurator->parse_args(
+        array_slice($GLOBALS['argv'], 1));
 
 
       $this->arguments = $args;
       $this->assoc_args = $assoc_args;
 
-      $configurator->merge_array( $runtime_config );
+      $configurator->merge_array($runtime_config);
     }
 
-    list( $this->config, $this->extra_config ) = $configurator->to_array();
+    list($this->config, $this->extra_config) = $configurator->to_array();
   }
 
   public function run() {
-    $this->init_config();
-    $this->init_colorization();
-    $this->init_logger();
-
-    if ( Terminus::is_test() )
+    if (Terminus::is_test())
       return true;
 
-    if ( empty( $this->arguments ) )
+    if (empty($this->arguments))
       $this->arguments[] = 'help';
 
     // Load bundled commands early, so that they're forced to use the same
     // APIs as non-bundled commands.
-    Utils\load_command( $this->arguments[0] );
+    Utils\load_command($this->arguments[0]);
 
-    if ( isset( $this->config['require'] ) ) {
-      foreach ( $this->config['require'] as $path ) {
-        Utils\load_file( $path );
+    if (isset($this->config['require'])) {
+      foreach ($this->config['require'] as $path) {
+        Utils\load_file($path);
       }
     }
 
     // Show synopsis if it's a composite command.
-    $r = $this->find_command_to_run( $this->arguments );
-    if ( is_array( $r ) ) {
-      list( $command ) = $r;
+    $r = $this->find_command_to_run($this->arguments);
+    if (is_array($r)) {
+      list($command) = $r;
 
-      if ( $command->can_have_subcommands() ) {
+      if ($command->can_have_subcommands()) {
         $command->show_usage();
         exit;
       }
     }
 
     // First try at showing man page
-    if ( 'help' === $this->arguments[0] && ( isset( $this->arguments[1] ) ) ) {
+    if ('help' === $this->arguments[0] && (isset($this->arguments[1]))) {
       $this->_run_command();
     }
 
     # Run the stinkin command!
     $this->_run_command();
-
   }
 
 }
