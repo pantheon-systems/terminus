9a056e3204db6eb48ceea60c0099180eecc93b2c
tesladethray <tesladethray@gmail.com>
2015-10-12 14:00:00 -0700

    Linted php/boot-fs.php

--- php/boot-fs.php
+++ php/boot-fs.php
@@ -3,15 +3,15 @@
 /**
  * This file needs to parse without error in PHP < 5.3
  */
-if(PHP_SAPI != 'cli') {
+if (PHP_SAPI != 'cli') {
   echo "Only CLI access.\n";
   die(-1);
 }
 
-if(version_compare(PHP_VERSION, '5.3.0', '<')) {
+if (version_compare(PHP_VERSION, '5.3.0', '<')) {
   printf(
-    "Error: Terminus requires PHP %s or newer. You are running version %s.\n", 
-    '5.3.0', 
+    "Error: Terminus requires PHP %s or newer. You are running version %s.\n",
+    '5.3.0',
     PHP_VERSION
   );
   die(-1);
