833ebf2391e4b3bbf26026a7362ec61b754a67a4
tesladethray <tesladethray@gmail.com>
2015-10-15 14:58:05 -0700

    Linted php/Terminus/Auth.php

--- php/Terminus/Auth.php
+++ php/Terminus/Auth.php
@@ -7,10 +7,18 @@ use Terminus\Exceptions\TerminusException;
 
 class Auth {
 
+  /**
+   * Determines if user is logged in
+   *
+   * @return [boolean] True if user is logged in
+   */
   public static function loggedIn() {
     if (Session::instance()->getValue('session', false) === false) {
-      throw new TerminusException("Please login first with `terminus auth login`");
+      throw new TerminusException(
+        'Please login first with `terminus auth login`'
+      );
     }
+    return true;
   }
 
 }
