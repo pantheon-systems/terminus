176081315ad7a19f4d8973e96f8b6be67d16945e
tesladethray <tesladethray@gmail.com>
2015-10-19 14:53:26 -0700

    Linted php/Terminus/Session.php

--- php/Terminus/Session.php
+++ php/Terminus/Session.php
@@ -8,60 +8,104 @@ class Session {
   static $instance;
   protected $data;
 
-
+  /**
+   * Instantiates object, sets session data
+   *
+   * @return [Session] $this
+   */
   public function __construct() {
-
-    $cache = Terminus::getCache();
+    $cache   = Terminus::getCache();
     $session = $cache->get_data('session');
 
-
+    $this->data = $session;
     if (empty($session)) {
-      $this->data = new \stdClass();;
-    } else {
-      $this->data = $session;
+      $this->data = new \stdClass();
     }
 
     self::$instance = $this;
     return $this;
   }
 
+  /**
+   * Retruns given data property or default if DNE.
+   *
+   * @param [string] $key     Name of property to return
+   * @param [mixed]  $default Default return value in case property DNE
+   * @return [mixed] $this->data->$key or $default
+   */
   public function get($key = 'session', $default = false) {
-    if ( isset($this->data) AND isset($this->data->$key) )
+    if (isset($this->data) && isset($this->data->$key)) {
       return $this->data->$key;
+    }
     return $default;
   }
 
+  /**
+   * Sets a keyed value to be part of the data property object
+   *
+   * @param [string] $key   Name of data property
+   * @param [mixed]  $value Value of property to set
+   * @return [Session] $this
+   */
   public function set($key, $value) {
-    $this->data->$key = !empty($value) ? $value : null;
+    $this->data->$key = null;
+    if (!empty($value)) {
+      $this->data->$key = $value;
+    }
     return $this;
   }
 
-  public static function instance( $session = null ) {
+  /**
+   * Retrieves session data
+   *
+   * @return [array] $data
+   */
+  public static function getData() {
+    $session = Session::instance();
+    return $session->data;
+  }
+
+  /**
+   * Returnes session data indicated by the key
+   *
+   * @param [string] $key Name of session property to retrieve
+   * @return [mixed] $session_property
+   */
+  public static function getValue($key) {
+    $session          = Session::instance();
+    $session_property = $session->get($key);
+    return $session_property;
+  }
+
+  /**
+   * Returns self, instantiating self if necessary
+   *
+   * @return [Session] self::$instance Static version of $this
+   */
+  public static function instance() {
     if (!self::$instance) {
       self::$instance = new self($session);
     }
     return self::$instance;
   }
 
-  public static function getValue( $key, $default = false ) {
-    $session = Session::instance();
-    return $session->get($key);
-  }
-
-  public static function getData() {
-    $session = Session::instance();
-    return $session->data;
-  }
-
-  public static function setData( $data ) {
+  /**
+   * Saves session data to cache
+   *
+   * @param [array] $data Session data to save
+   * @return [void]
+   */
+  public static function setData($data) {
     $cache = Terminus::getCache();
     Terminus::getLogger()->info('Saving session data');
     $cache->put_data('session', $data);
     $session = self::instance();
-    $session->set('data',$data);
-    if (empty($data)) return false;
-    foreach ($data as $k=>$v) {
-        $session->set($k,$v);
+    $session->set('data', $data);
+    if (empty($data)) {
+      return false;
+    }
+    foreach ($data as $k => $v) {
+      $session->set($k, $v);
     }
   }
 
