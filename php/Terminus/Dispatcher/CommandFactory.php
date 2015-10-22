d74f6b2ad4b90b79caa1f6a80463aac60f8914f0
tesladethray <tesladethray@gmail.com>
2015-10-21 17:20:41 -0700

    Linted php/Terminus/Dispatcher/CommandFactory.php

--- php/Terminus/Dispatcher/CommandFactory.php
+++ php/Terminus/Dispatcher/CommandFactory.php
@@ -10,58 +10,108 @@ use Terminus\Dispatcher\CompositeCommand;
  */
 class CommandFactory {
 
-  public static function create( $name, $class, $parent ) {
-    $reflection = new \ReflectionClass( $class );
+  /**
+   * Creates a new composite command or subcommand
+   *
+   * @param [string]      $name   Name of command to create
+   * @param [string]      $class  Name of class command belongs to
+   * @param [RootCommand] $parent Parent command
+   * @return [mixed] $command Either CompositeCommand or Subcommand
+   */
+  public static function create($name, $class, $parent) {
+    $reflection = new \ReflectionClass($class);
 
-    if ( $reflection->hasMethod( '__invoke' ) ) {
-      $command = self::create_subcommand( $parent, $name, $reflection->name,
-        $reflection->getMethod( '__invoke' ) );
+    if ($reflection->hasMethod('__invoke')) {
+      $command = self::createSubcommand(
+        $parent,
+        $name,
+        $reflection->name,
+        $reflection->getMethod('__invoke')
+      );
     } else {
-      $command = self::create_composite_command( $parent, $name, $reflection );
+      $command = self::createCompositeCommand($parent, $name, $reflection);
     }
 
     return $command;
   }
 
-  private static function create_subcommand( $parent, $name, $class_name, $method ) {
-    $docparser = new DocParser( $method->getDocComment() );
+  /**
+   * Creates a new composite command
+   *
+   * @param [RootCommand]      $parent     Parent command
+   * @param [string]           $name       Name of command to create
+   * @param [\ReflectionClass] $reflection Object with name of class to call
+   * @return [CompositeCommand] $container
+   */
+  private static function createCompositeCommand($parent, $name, $reflection) {
+    $docparser = new DocParser($reflection->getDocComment());
+    $container = new CompositeCommand($parent, $name, $docparser);
 
-    if ( !$name )
-      $name = $docparser->getTag( 'subcommand' );
-
-    if ( !$name )
-      $name = $method->name;
-
-    $method_name = $method->name;
-
-    $when_invoked = function ( $args, $assoc_args ) use ( $class_name, $method_name ) {
-      call_user_func( array( new $class_name, $method_name ), $args, $assoc_args );
-    };
-
-    return new Subcommand( $parent, $name, $docparser, $when_invoked );
-  }
-
-  private static function create_composite_command( $parent, $name, $reflection ) {
-    $docparser = new DocParser( $reflection->getDocComment() );
-
-    $container = new CompositeCommand( $parent, $name, $docparser );
-
-    foreach ( $reflection->getMethods() as $method ) {
-      if ( !self::is_good_method( $method ) )
+    foreach ($reflection->getMethods() as $method) {
+      if (!self::isGoodMethod($method)) {
         continue;
-
-      $subcommand = self::create_subcommand( $container, false, $reflection->name, $method );
-
+      }
+      $subcommand      = self::createSubcommand(
+        $container,
+        false,
+        $reflection->name,
+        $method
+      );
       $subcommand_name = $subcommand->get_name();
-
-      $container->add_subcommand( $subcommand_name, $subcommand );
+      $container->add_subcommand($subcommand_name, $subcommand);
     }
 
     return $container;
   }
 
-  private static function is_good_method( $method ) {
-    return $method->isPublic() && !$method->isConstructor() && !$method->isStatic();
+  /**
+   * Creates a new subcommand
+   *
+   * @param [RootCommand] $parent     Parent command
+   * @param [string]      $name       Name of command to create
+   * @param [string]      $class_name Name of class command belongs to
+   * @param [string]      $method     Name of function to invoke in class
+   * @return [Subcommand] $subcommand
+   */
+  private static function createSubcommand(
+    $parent,
+    $name,
+    $class_name,
+    $method
+  ) {
+    $docparser = new DocParser($method->getDocComment());
+    if (!$name) {
+      $name = $docparser->getTag('subcommand');
+      if (!$name) {
+        $name = $method->name;
+      }
+    }
+    $method_name  = $method->name;
+    $when_invoked =
+      function ($args, $assoc_args) use ($class_name, $method_name) {
+        call_user_func(
+          array(new $class_name, $method_name),
+          $args,
+          $assoc_args
+        );
+      };
+
+    $subcommand = new Subcommand($parent, $name, $docparser, $when_invoked);
+    return $subcommand;
   }
+
+  /**
+   * Decides if the given method is a good one
+   *
+   * @param [string] $method Name of method to be called
+   * @return [boolean] $is_good_method
+   */
+  private static function isGoodMethod($method) {
+    $is_good_method = $method->isPublic()
+      && !$method->isConstructor()
+      && !$method->isStatic();
+    return $is_good_method;
+  }
+
 }
 
