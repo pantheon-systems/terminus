46a8226587fb1764f97332f96009d8f41e71f7b5
tesladethray <tesladethray@gmail.com>
2015-10-22 11:56:09 -0700

    Linted php/Terminus/Dispatcher/RootCommand.php

--- php/Terminus/Dispatcher/RootCommand.php
+++ php/Terminus/Dispatcher/RootCommand.php
@@ -10,43 +10,25 @@ use Terminus\Utils;
  */
 class RootCommand extends CompositeCommand {
 
+  /**
+   * Object constructor, sets object properties
+   *
+   * @return [RootCommand] $this
+   */
   function __construct() {
-    $this->parent = false;
-    $this->name = 'terminus';
+    $this->parent    = false;
+    $this->name      = 'terminus';
     $this->shortdesc = 'Manage Pantheon through the command-line.';
   }
 
-  function getLongdesc() {
-    $binding = array();
-
-    foreach (Terminus::getConfigurator()->getSpec() as $key => $details) {
-      if (
-        ($details['runtime'] === false)
-        || isset($details['deprecated'])
-        || (isset($details['hidden']))
-      ) {
-        continue;
-      } if ($details['runtime']) {
-        $synopsis = "--[no-]$key"; 
-      } else {
-        $synopsis = "--$key" . $details['runtime'];
-      }
-
-      $binding['parameters'][] = array(
-        'synopsis' => $synopsis,
-        'desc'     => $details['desc']
-     );
-    }
-
-    if (Terminus::getConfig('format') == 'json') {
-      return $binding;
-    }
-    return Utils\mustacheRender('man-params.mustache', $binding);
-  }
-
+  /**
+   * Finds a subcommand of the root command
+   *
+   * @param [array] $args Arguments to parse into subcommands
+   * @return [mixed] $this->$subcommands[$command] or false if DNE
+   */
   function findSubcommand(&$args) {
     $command = array_shift($args);
-
     Utils\loadCommand($command);
 
     if (!isset($this->subcommands[$command])) {
@@ -56,10 +38,48 @@ class RootCommand extends CompositeCommand {
     return $this->subcommands[$command];
   }
 
+  /**
+   * Returns long description of this command by parsing the docs
+   *
+   * @return [string] $binding
+   */
+  function getLongdesc() {
+    $binding = array();
+
+    foreach (Terminus::getConfigurator()->getSpec() as $key => $details) {
+      if (($details['runtime'] === false)
+        || isset($details['deprecated'])
+        || (isset($details['hidden']))
+      ) {
+        continue;
+      } if ($details['runtime']) {
+        $synopsis = "--[no-]$key";
+      } else {
+        $synopsis = "--$key" . $details['runtime'];
+      }
+
+      $binding['parameters'][] = array(
+        'synopsis' => $synopsis,
+        'desc'     => $details['desc']
+      );
+    }
+
+    if (Terminus::getConfig('format') == 'json') {
+      return $binding;
+    }
+    $binding = Utils\mustacheRender('man-params.mustache', $binding);
+    return $binding;
+  }
+
+  /**
+   * Returns all subcommands of the root command
+   *
+   * @return [array] $subcommands An array of Subcommand objects
+   */
   function getSubcommands() {
     Utils\loadAllCommands();
-
-    return parent::getSubcommands();
+    $subcommands = parent::getSubcommands();
+    return $subcommands;
   }
-}
 
+}
