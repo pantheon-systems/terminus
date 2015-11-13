f543d934c8d9c7e060a20623caa9642b0473d09a
tesladethray <tesladethray@gmail.com>
2015-11-13 13:06:49 -0800

    Linted php/Terminus/Outputters/OutputterInterface.php

--- php/Terminus/Outputters/OutputterInterface.php
+++ php/Terminus/Outputters/OutputterInterface.php
@@ -1,10 +1,10 @@
 <?php
+
 /**
  * @file
  */
 
 namespace Terminus\Outputters;
-
 
 /**
  * Interface OutputterInterface
@@ -13,70 +13,65 @@ namespace Terminus\Outputters;
 interface OutputterInterface {
 
   /**
-   * Set the writer which sends the output to it's final destination.
+   * Outputs any variable type as a raw dump
    *
-   * @param OutputWriterInterface $writer
+   * @param [object|array] $object Item to dump information on
+   * @return [void]
    */
-  public function setWriter(OutputWriterInterface $writer);
+  public function outputDump($object);
 
   /**
-   * Set the formatter which converts the output to a useful string.
+   * Formats a single record or object
    *
-   * @param OutputFormatterInterface $formatter
-   */
-  public function setFormatter(OutputFormatterInterface $formatter);
-
-  /**
-   * Formats a single scalar value with an optional human label.
-   *
-   * @param mixed $value
-   *  The scalar value to format
-   * @param string $human_label
-   *  The human readable label for the value
-   * @return string
-   */
-  public function outputValue($value, $human_label = '');
-
-  /**
-   * Format a single record or object
-   *
-   * @param array|object $record
-   *   A key/value array or object
-   * @param array $human_labels
-   *   A key/value array mapping the keys in the record to human labels
-   * @return string
+   * @param [array|object] $record       A key/value array or object
+   * @param [array]        $human_labels A key/value array mapping the keys in
+   *   the record to human labels
+   * @return [void]
    */
   public function outputRecord($record, $human_labels = array());
 
-
   /**
-   * Format a list of scalar values
+   * Formats a list of records of the same type
    *
-   * @param array $values
-   *  The values to format
-   * @param string $human_label
-   *  A human name for the entire list. If each value needs a separate label then
-   *  formatRecord should be used.
-   * @return string
-   */
-  public function outputValueList($values, $human_label = '');
-
-  /**
-   * Format a list of records of the same type.
-   *
-   * @param array $records
-   *  A list of arrays or objects.
-   * @param array $human_labels
-   *  An array that maps the record keys to human names.
-   * @return string
+   * @param [array] $records      A list of arrays or objects.
+   * @param [array] $human_labels An array that maps record keys to human names
+   * @return [void]
    */
   public function outputRecordList($records, $human_labels = array());
 
   /**
-   * Output any variable type as a raw dump.
+   * Formats a single scalar value with an optional human label
    *
-   * @param $object
-   * @return string
+   * @param [mixed]  $value       The scalar value to format
+   * @param [string] $human_label The human readable label for the value
+   * @return [void]
    */
-  public function outputDump($object);
+  public function outputValue($value, $human_label = '');
+
+  /**
+   * Formats a list of scalar values
+   *
+   * @param [array]  $values      The values to format
+   * @param [string] $human_label One human name for the entire list. If each
+   *   value needs a separate label, then formatRecord should be used.
+   * @return [void]
+   */
+  public function outputValueList($values, $human_label = '');
+
+  /**
+   * Sets the formatter which converts the output to a useful string
+   *
+   * @param [OutputFormatterInterface] $formatter Formatter selected for use
+   * @return [void]
+   */
+  public function setFormatter(OutputFormatterInterface $formatter);
+
+  /**
+   * Sets the writer which sends the output to it's final destination
+   *
+   * @param [OutputWriterInterface] $writer Writer selected for use
+   * @return [void]
+   */
+  public function setWriter(OutputWriterInterface $writer);
+
 }
