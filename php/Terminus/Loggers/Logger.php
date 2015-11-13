ceb623a128811b2714dca62a553b39ea23ba07a8
tesladethray <tesladethray@gmail.com>
2015-11-13 14:11:43 -0800

    Linted php/Terminus/Loggers/Logger.php

--- php/Terminus/Loggers/Logger.php
+++ php/Terminus/Loggers/Logger.php
@@ -38,10 +38,9 @@ class Logger extends KLogger {
     } elseif ($config['format'] == 'silent') {
       $logDirectory = ini_get('error_log');
       if ($logDirectory == '') {
-        die(
-          'You must either set error_log in your php.ini, or define '
-          . ' TERMINUS_LOG_DIR to use silent mode.' . PHP_EOL
-        );
+        $message  = 'You must either set error_log in your php.ini, or define ';
+        $message .= ' TERMINUS_LOG_DIR to use silent mode.' . PHP_EOL;
+        die($message);
       }
     }
 
@@ -49,30 +48,29 @@ class Logger extends KLogger {
   }
 
   /**
-    * Logs with an arbitrary level.
+    * Logs with an arbitrary level
     *
-    * @param [mixed]  $level
-    * @param [string] $message
-    * @param [array]  $context
+    * @param [mixed]  $level   PSR log level of message
+    * @param [string] $message Message to give
+    * @param [array]  $context Context of message
     * @return [void]
     */
   public function log($level, $message, array $context = array()) {
-    if (
-      isset($this->logLevelThreshold)
+    if (isset($this->logLevelThreshold)
       && ($this->logLevels[$this->logLevelThreshold] < $this->logLevels[$level])
     ) {
       return;
     }
 
     // Replace the context variables into the message per PSR spec:
-    // https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-3-logger-interface.md#12-message
+    // https://github.com/php-fig/fig-standards/blob/master/accepted/...
+    //   ...PSR-3-logger-interface.md#12-message
     $message = $this->interpolate($message, $context);
 
-
     if (isset($this->options) && $this->options['logFormat'] == 'json') {
-      $message = $this->formatJsonMessages($level, $message, $context);
+      $message = $this->formatJsonMessages($level, $message);
     } elseif (isset($this->options) && $this->options['logFormat'] == 'bash') {
-      $message = $this->formatBashMessages($level, $message, $context);
+      $message = $this->formatBashMessages($level, $message);
     } else {
       $message = $this->formatMessage($level, $message, $context);
     }
@@ -85,8 +83,38 @@ class Logger extends KLogger {
    * @return [void]
    */
   public function setBufferHandle() {
-    $handle_name = strtoupper(substr($this->getLogFilePath(), 6));
+    $handle_name      = strtoupper(substr($this->getLogFilePath(), 6));
     $this->fileHandle = constant($handle_name);
+  }
+
+  /**
+    * Formats the message for logging.
+    *
+    * @param  [string] $level   The Log Level of the message
+    * @param  [string] $message The message to log
+    * @param  [array]  $context The context
+    * @return [string] $message
+    */
+  protected function formatMessage($level, $message, $context) {
+    if (isset($this->options)
+      && in_array($this->options['logFormat'], array('bash', 'json'))
+    ) {
+      $parts   = $this->getMessageParts($level, $message);
+      $message = $this->options['logFormat'];
+      foreach ($parts as $part => $value) {
+        $message = str_replace('{'.$part.'}', $value, $message);
+      }
+    } else {
+      $message = "[{$this->getTimestamp()}] [$level] $message";
+    }
+    if (isset($this->options)
+      && $this->options['appendContext']
+      && ! empty($context)
+    ) {
+      $message .= PHP_EOL . $this->indent($this->contextToString($context));
+    }
+
+    return $message . PHP_EOL;
   }
 
   /**
@@ -94,11 +122,10 @@ class Logger extends KLogger {
     *
     * @param  [string] $level   The Log Level of the message
     * @param  [string] $message The message to log
-    * @param  [array]  $context The context
     * @return [string] $message
     */
-  private function formatBashMessages($level, $message, $context) {
-    $parts   = $this->getMessageParts($level, $message, $context);
+  private function formatBashMessages($level, $message) {
+    $parts   = $this->getMessageParts($level, $message);
     $message = '';
     foreach ($parts as $key => $value) {
       $message .= "$key\t$value\n";
@@ -111,45 +138,12 @@ class Logger extends KLogger {
     *
     * @param  [string] $level   The Log Level of the message
     * @param  [string] $message The message to log
-    * @param  [array]  $context The context
     * @return [string] $message
     */
-  private function formatJsonMessages($level, $message, $context) {
-    $parts   = $this->getMessageParts($level, $message, $context);
+  private function formatJsonMessages($level, $message) {
+    $parts   = $this->getMessageParts($level, $message);
     $message = json_encode($parts) . "\n";
     return $message;
-  }
-
-  /**
-    * Formats the message for logging.
-    *
-    * @param  [string] $level   The Log Level of the message
-    * @param  [string] $message The message to log
-    * @param  [array]  $context The context
-    * @return [string] $message
-    */
-  protected function formatMessage($level, $message, $context) {
-    if (
-      isset($this->options)
-      && in_array($this->options['logFormat'], array('bash', 'json'))
-    ) {
-      $parts   = $this->getMessageParts($level, $message, $context);
-      $message = $this->options['logFormat'];
-      foreach ($parts as $part => $value) {
-        $message = str_replace('{'.$part.'}', $value, $message);
-      }
-    } else {
-      $message = "[{$this->getTimestamp()}] [$level] $message";
-    }
-    if (
-      isset($this->options)
-      && $this->options['appendContext']
-      && ! empty($context)
-    ) {
-      $message .= PHP_EOL . $this->indent($this->contextToString($context));
-    }
-
-    return $message . PHP_EOL;
   }
 
   /**
@@ -157,10 +151,9 @@ class Logger extends KLogger {
     *
     * @param  [string] $level   The Log Level of the message
     * @param  [string] $message The message to log
-    * @param  [array]  $context The context
     * @return [string] $parts
     */
-  private function getMessageParts($level, $message, $context) {
+  private function getMessageParts($level, $message) {
     $parts = array(
       'date'          => $this->getTimestamp(),
       'level'         => strtoupper($level),
@@ -186,11 +179,11 @@ class Logger extends KLogger {
   }
 
   /**
-   * Interpolates context variables per the PSR spec.
+   * Interpolates context variables per the PSR spec
    *
-   * @param string $message The message containing replacements in the form {key}
-   * @param array $context The array containing the values to be substituted.
-   * @return string
+   * @param [string] $message The message containing curly brace-enclosed keys
+   * @param [array]  $context The array containing substitutionary values
+   * @return [string] $interpolated_string
    */
   private function interpolate($message, $context) {
     // build a replacement array with braces around the context keys
@@ -200,10 +193,11 @@ class Logger extends KLogger {
     }
 
     // interpolate replacement values into the message and return
-    if(!is_string($message)) {
+    if (!is_string($message)) {
       $message = json_encode($message);
     }
-    return strtr($message, $replace);
+    $interpolated_string = strtr($message, $replace);
+    return $interpolated_string;
   }
 
 }
