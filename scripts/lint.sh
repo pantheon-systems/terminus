0ed8344d151b9ae515d70a8148436f3b667bf3c3
tesladethray <tesladethray@gmail.com>
2015-11-13 16:00:36 -0800

    Added linting of non-command files to test suite

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,5 @@
 #Run PHP Copy-Paste Detector
 vendor/bin/phpcpd php
 
-#Run PHP Code Sniffer
-phpcpd_cmd="vendor/bin/phpcs --standard=tests/config/standards.xml --extensions=php"
-if [ ! -z $1 ]; then phpcpd_cmd+=" --severity=0"; fi
-phpcpd_cmd+=" php"
-eval $phpcpd_cmd
+#Run PHP Code Sniffer on non-command files
+vendor/bin/phpcs --standard=tests/config/standards.xml --extensions=php --warning-severity=6 --error-severity=1 --ignore=php/commands php/*
