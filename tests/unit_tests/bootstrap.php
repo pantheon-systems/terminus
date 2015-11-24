0252c8e4709e4cb885a9c7ffd8c5f608b9d38209
tesladethray <tesladethray@gmail.com>
2015-11-23 16:33:11 -0800

    Linted tests/unit_tests/*

--- tests/unit_tests/bootstrap.php
+++ tests/unit_tests/bootstrap.php
@@ -3,9 +3,9 @@
 /**
  * Bootstrap file for unit tests
  */
-define('CLI_ROOT', dirname(__DIR__) . '/..' );
+define('CLI_ROOT', dirname(__DIR__) . '/..');
 define('TEST_DIR', dirname(__DIR__));
-define('TERMINUS_CMD','php ' . CLI_ROOT . '/php/boot-fs.php');
+define('TERMINUS_CMD', 'php ' . CLI_ROOT . '/php/boot-fs.php');
 putenv('CLI_TEST_MODE=1');
 
 require_once CLI_ROOT . '/vendor/autoload.php';
@@ -13,17 +13,23 @@ require_once CLI_ROOT . '/php/boot-fs.php';
 Terminus::setConfig('debug', false);
 use Terminus\Session;
 
+$session_id  = '0ffec038-4410-43d0-a404-46997f672d7a%3A68486878';
+$session_id .= '-dd87-11e4-b243-bc764e1113b5%3AbQR2fyNMh5PQXN6F2Ewge';
 // Set some dummy credentials
-Session::setData(json_decode('{
-  "user_uuid": "0ffec038-4410-43d0-a404-46997f672d7a",
-  "session": "0ffec038-4410-43d0-a404-46997f672d7a%3A68486878-dd87-11e4-b243-bc764e1113b5%3AbQR2fyNMh5PQXN6F2Ewge",
-  "session_expire_time": 1739299351,
-  "email": "bensheldon+pantheontest@gmail.com"
-}'));
+Session::setData(
+  json_decode(
+    '{
+      "user_uuid": "0ffec038-4410-43d0-a404-46997f672d7a",
+      "session": "' . $session_id . '",
+      "session_expire_time": 1739299351,
+      "email": "bensheldon+pantheontest@gmail.com"
+    }'
+  )
+);
 
 \VCR\VCR::configure()->enableRequestMatchers(array('method', 'url', 'body'));
 
-# Prevent API requests from being made in CI Environment
+// Prevent API requests from being made in CI Environment
 $ci_environment = getenv('CI');
 if ($ci_environment) {
   \VCR\VCR::configure()->setMode('none');
--- tests/unit_tests/test-upstreams.php
+++ tests/unit_tests/test-upstreams.php
@@ -1,17 +1,18 @@
 <?php
 
-use \Terminus\Models\Collections\Upstreams;
+use Terminus\Models\Collections\Upstreams;
 
 class UpstreamsTest extends PHPUnit_Framework_TestCase {
 
   /**
    * @vcr upstreams_instance
    */
-  function testUpstreamsInstance() {
+  public function testUpstreamsInstance() {
     $upstreams = new Upstreams(array());
     $test      = $upstreams->get('3b754bc2-48f8-4388-b5b5-2631098d03de');
     $this->assertEquals('CiviCRM Starter Kit', $test->get('longname'));
     $test = $upstreams->all();
     $this->assertNotEmpty($test);
   }
+
 }
