12c069994ff412e51ea451d25b2be3a0cf778350
tesladethray <tesladethray@gmail.com>
2015-11-30 16:04:16 -0800

    Forcing exit upon linter error

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,6 +1,7 @@
 #Run PHP Copy-Paste Detector
 vendor/bin/phpcpd php
 
+set -e
 phpcs="vendor/bin/phpcs --standard=tests/config/standards.xml --extensions=php --warning-severity=6 --error-severity=1"
 #Run PHP Code Sniffer on files with internal documentation
 cmd=$phpcs+" --ignore=php/Terminus/Commands/*,vendor/*,tests/unit_tests/*,tests/features/bootstrap/FeatureContext.php *"
