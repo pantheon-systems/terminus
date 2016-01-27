Deleriumdoll/cli

Adds list and delete commands for machine tokens

php/Terminus/Models/MachineToken.php
@@ -1,0 +1,19 @@
+<?php
+
+namespace Terminus\Models;
+
+class MachineToken extends TerminusModel {
+
+  /**
+   * Deletes machine token
+   *
+   * @return array
+   */
+  public function delete() {
+    $response = $this->request->simpleRequest(
+      'users/' . $this->user->id . '/refresh_tokens/' . $this->get('id'),
+      array('method' => 'delete')
+    );
+    return $response;
+  }
+}

pantheon-systems/terminus

php/Terminus/Models/MachineToken.php
@@ -1,0 +1,20 @@
+<?php
+
+namespace Terminus\Models;
+
+class MachineToken extends TerminusModel {
+
+  /**
+   * Deletes machine token
+   *
+   * @return array
+   */
+  public function delete() {
+    $response = $this->request->simpleRequest(
+      'users/' . $this->user->id . '/machine_tokens/' . $this->get('id'),
+      array('method' => 'delete')
+    );
+    return $response;
+  }
+
+}