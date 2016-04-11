b54b2c679c55bdedf67f5d44b24fd204bd9154ca
Ed Reel <edreel@gmail.com>
2016-04-11 16:12:46 -0500

    Added Mac and Linux support to utils.php
    All DRY-ed up.

--- php/utils.php
+++ php/utils.php
@@ -17,6 +17,50 @@ function isTest() {
   }
   return $is_test;
 }
+/**
+ * Check whether Terminus is running in a certain OS environment
+ *
+ * @return bool True if OS running Terminus is based on argument passed
+ */
+function isOS($test_os = '') {
+  $test_os = strtoupper($test_os);
+  $os = strtoupper(substr(PHP_OS, 0, 3));
+  switch ($test_os) {
+    case 'MAC':
+      $is_os = ($os == 'DAR');
+      break;
+    case 'LINUX':
+      $is_os = ($os == 'LIN');
+      break;
+    case 'WINDOWS':
+      $is_os = ($os == 'WIN');
+      break;
+    default:
+      $is_os = false;
+  }
+  if ((boolean)getenv('TERMINUS_TEST_IGNORE')) {
+    $is_os = !$is_os;
+  }
+  return $is_os;
+}
+
+/**
+ * Check whether Terminus is running in a Mac environment
+ *
+ * @return bool True if OS running Terminus is Mac
+ */
+function isMac() {
+  return $this->isOS('Mac');
+}
+
+/**
+ * Check whether Terminus is running in a Linux environment
+ *
+ * @return bool True if OS running Terminus is Linux
+ */
+function isLinux() {
+  return $this->isOS('Linux');
+}
 
 /**
  * Check whether Terminus is running in a Windows environment
@@ -24,9 +68,5 @@ function isTest() {
  * @return bool True if OS running Terminus is Windows
  */
 function isWindows() {
-  $is_windows = strtoupper(substr(PHP_OS, 0, 3)) == 'WIN';
-  if ((boolean)getenv('TERMINUS_TEST_IGNORE')) {
-    $is_windows = !$is_windows;
-  }
-  return $is_windows;
+  return $this->isOS('Windows');
 }
