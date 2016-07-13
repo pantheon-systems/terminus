477754d1b174f2e8b885ad89febb5418513b816f
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-07-13 15:52:48 -0400

    Allow auto-fix from linter

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,8 +1,23 @@
-#Run PHP Copy-Paste Detector
+# Run PHP Copy-Paste Detector
 vendor/bin/phpcpd php
 
 set -e
-phpcs="vendor/bin/phpcs --standard=tests/config/standards.xml --extensions=php --warning-severity=6 --error-severity=1"
+
+executable=phpcs
+
+# optionally fix stuff instead of just report it.
+while getopts ":f" opt; do
+  case $opt in
+    f)
+      executable=phpcbf
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+phpcs="vendor/bin/$executable --standard=tests/config/standards.xml --extensions=php --warning-severity=6 --error-severity=1"
 #Run PHP Code Sniffer on files with internal documentation
 cmd=$phpcs+" --ignore=php/Terminus/Commands/*,vendor/*,tests/unit_tests/*,tests/features/bootstrap/FeatureContext.php *"
 eval $cmd
@@ -12,11 +27,11 @@ cmd=$phpcs+" php/Terminus/Commands/CommandWithSSH.php"
 eval $cmd
 
 #Run PHP Code Sniffer on command files using a standards subset excepting internal documentation
-phpcs="vendor/bin/phpcs --standard=tests/config/command_standards.xml --extensions=php --warning-severity=6 --error-severity=1"
+phpcs="vendor/bin/phpcbf --standard=tests/config/command_standards.xml --extensions=php --warning-severity=6 --error-severity=1"
 cmd=$phpcs+" --ignore=php/Terminus/Commands/TerminusCommand.php,php/Terminus/Command/CommandWithSSH.php php/Terminus/Commands/* tests/unit_tests/*"
 eval $cmd
 
 #Run PHP Code Sniffer on command files using a standards subset excepting long lines
-phpcs="vendor/bin/phpcs --standard=tests/config/context_standards.xml --extensions=php --warning-severity=6 --error-severity=1"
+phpcs="vendor/bin/phpcbf --standard=tests/config/context_standards.xml --extensions=php --warning-severity=6 --error-severity=1"
 cmd=$phpcs+" tests/features/bootstrap/FeatureContext.php"
 eval $cmd
