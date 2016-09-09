2ac617ad0309cbc8ec48f8a18020cfcbbaf3471d
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-09-08 21:21:33 -0400

    Add Initial Robofile with sniff command.

---
+++ scripts/RoboFile.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * This is project's console commands configuration for Robo task runner.
+ *
+ * @see http://robo.li/
+ */
+class RoboFile extends \Robo\Tasks
+{
+  const PROJECT_ROOT = __DIR__ . '/../';
+
+  /**
+   * Code sniffer.
+   *
+   * Run the PHP Codesniffer on a file or directory.
+   *
+   * @param string $file
+   *    A file or directory to analyze.
+   * @option $autofix Whether to run the automatic fixer or not.
+   * @option $strict Show warnings as well as errors.
+   *    Default is to show only errors.
+   *
+   * @return \Robo\Result
+   *
+   */
+  public function sniff(
+    $file = '../src/',
+    $options = [
+      'autofix' => false,
+      'strict' => false,
+    ]
+  ) {
+    $strict = $options['strict'] ? '' : '-n';
+    $result = $this->taskExec(self::PROJECT_ROOT . "vendor/bin/phpcs --standard=PSR2 {$strict} {$file}")->run();
+    if (!$result->wasSuccessful()) {
+      if (!$options['autofix']) {
+        $options['autofix'] = $this->confirm('Would you like to run phpcbf to fix the reported errors?');
+      }
+      if ($options['autofix']) {
+        $result = $this->taskExec(self::PROJECT_ROOT . "vendor/bin/phpcbf --standard=PSR2 {$file}")->run();
+      }
+    }
+    return $result;
+  }
+}
