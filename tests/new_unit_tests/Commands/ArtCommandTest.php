3e47e37ab253e6a511a570376d165668fe0d23dd
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2016-09-14 11:47:09 -0400

    Add Command test helper

--- tests/new_unit_tests/Commands/ArtCommandTest.php
+++ tests/new_unit_tests/Commands/ArtCommandTest.php
@@ -2,7 +2,6 @@
 namespace Pantheon\Terminus\UnitTests\Commands;
 
 use Pantheon\Terminus\Tests\CommandTestCase;
-use Symfony\Component\Console\Output\BufferedOutput;
 
 class ArtCommandTest extends CommandTestCase
 {
@@ -13,14 +12,20 @@ class ArtCommandTest extends CommandTestCase
         $this->setInput(['command' => 'art', 'name' => 'hello']);
     }
 
-    public function testArtPrintsArt()
+    /**
+     * @test
+     */
+    public function art_command_prints_contents_of_files_in_assets_directory()
     {
-        $this->assertEquals('Hello World!', $this->runCommand()->getOutput());
+        $this->assertEquals('Hello World!', $this->runCommand()->fetchTrimmedOutput());
     }
 
-    public function testArtRejectsNonExistantFiles()
+    /**
+     * @test
+     */
+    public function art_command_rejects_files_not_in_assets_directory()
     {
         $this->setInput(['command' => 'art', 'name' => 'foo']);
-        $this->assertEquals('Not a valid work of art!', $this->runCommand()->getOutput());
+        $this->assertEquals('Not a valid work of art!', $this->runCommand()->fetchTrimmedOutput());
     }
 }
