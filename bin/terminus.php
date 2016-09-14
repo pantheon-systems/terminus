5ee8e5f7fd8bcb18a63cd60d5abec3bd448080e4
Sara McCutcheon <tesladethray@gmail.com>
2016-09-14 13:59:15 -0700

    Added VCR configuration to CommandTestCase

--- bin/terminus.php
+++ bin/terminus.php
@@ -42,5 +42,4 @@ $container->inflector(SessionAwareInterface::class)
 // Running Terminus
 $runner = new Runner($container);
 $status_code = $runner->run($input, $output);
-
 exit($status_code);
--- src/Commands/ArtCommand.php
+++ src/Commands/ArtCommand.php
@@ -10,7 +10,6 @@ class ArtCommand extends TerminusCommand
      * Displays Pantheon ASCII artwork
      *
      * @name art
-     * @authorize
      *
      * @param string $name Name of the artwork to select
      * @usage terminus art rocket
