d65455b118a32c7b62bead11484b1ae5011bbe78
Ben Sheldon <bensheldon@gmail.com>
2016-12-16 14:32:02 -0800

    Update helptext for `site` commands

--- src/Commands/Site/LookupCommand.php
+++ src/Commands/Site/LookupCommand.php
@@ -11,24 +11,22 @@ use Consolidation\OutputFormatters\StructuredData\PropertyList;
 class LookupCommand extends SiteCommand
 {
     /**
-     * Look up a site by its name
+     * Displays the UUID of a site given its name.
      *
      * @authorize
      *
      * @command site:lookup
      *
      * @field-labels
-     *   id: ID
-     *   name: Name
+     *     id: ID
+     *     name: Name
      * @default-string-field id
      * @return PropertyList
      *
      * @param string $site_name Name of a site to look up
      *
      * @usage terminus site:lookup <site>
-     *   * Responds with the UUID of <site> if it exists and you have access to it
-     *   * Responds that you are forbidden if you access <site>, which exists, but you do not have access to it
-     *   * Responds that <site> does not exist
+     *     Displays the UUID of <site> if it exists and is accessible to you.
      */
     public function lookup($site_name)
     {
