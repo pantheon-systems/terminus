232bab254761298ad866d7a66bf444308a8d2107
Ed Reel <ed.reel@drupalconnect.com>
2017-01-02 10:47:57 -0600

    Refactored plugin commands into separate class files

---
+++ src/Commands/Plugin/UninstallCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Pantheon\Terminus\Commands\Plugin;
+
+use Pantheon\Terminus\Exceptions\TerminusNotFoundException;
+
+/**
+ * Manage Terminus plugins.
+ *
+ * @package Pantheon\Terminus\Commands\Plugin
+ */
+class UninstallCommand extends PluginBaseCommand
+{
+    /**
+     * Remove one or more Terminus plugins.
+     *
+     * TODO: Add the ability to prompt for plugins to remove.
+     *
+     * @command plugin:uninstall
+     * @aliases plugin:remove plugin:delete
+     *
+     * @option array $plugins A list of one or more installed plugins to remove
+     *
+     * @usage <plugin-name-1> [plugin-name-2] ...
+     */
+    public function uninstall(array $plugins)
+    {
+        if (empty($plugins)) {
+            $message = "Usage: terminus plugin:<uninstall|remove|delete>";
+            $message .= " <plugin-name-1> [plugin-name-2] ...";
+            throw new TerminusNotFoundException($message);
+        }
+
+        foreach ($plugins as $plugin) {
+            $plugin_dir = $this->getPluginDir($plugin);
+            if (!is_dir("$plugin_dir")) {
+                $message = "{$plugin} plugin is not installed.";
+                $this->log()->error($message);
+            } else {
+                exec("rm -rf \"$plugin_dir\"", $messages);
+                foreach ($messages as $message) {
+                    $this->log()->notice($message);
+                }
+                $message = "{$plugin} plugin was removed successfully.";
+                $this->log()->notice($message);
+            }
+        }
+    }
+}
