ee8ec22ec2d6a791832ed60c4541f0d459cf5354
Ed Reel <edreel@gmail.com>
2017-02-13 09:17:49 -0600

    Update for better Windows support
      - Move updatePlugin method from PluginBaseCommand.php to
      UpdateCommand.php
      - Remove getSlash method in PluginBaseCommand.php

--- src/Commands/Plugin/UpdateCommand.php
+++ src/Commands/Plugin/UpdateCommand.php
@@ -47,4 +47,67 @@ class UpdateCommand extends PluginBaseCommand
             $this->updatePlugin($plugin);
         }
     }
+
+    /**
+     * Update a specific plugin.
+     *
+     * @param string $plugin Plugin name
+     */
+    protected function updatePlugin($plugin)
+    {
+        $plugins_dir = $this->getPluginDir();
+        $plugin_dir = $plugins_dir . $plugin;
+        if (!is_dir("$plugin_dir")) {
+            $message = "{$plugin} is not installed.";
+            throw new TerminusNotFoundException($message);
+        }
+        $messages = array();
+        $message = "Updating {$plugin}...";
+        $this->log()->notice($message);
+        $method = $this->getInstallMethod($plugin);
+        switch ($method) {
+            case 'git':
+                // Compare installed version to the latest release.
+                $installed_version = $this->getInstalledVersion($plugin_dir);
+                $latest_version = $this->getLatestVersion($plugin_dir);
+                if ($installed_version != 'unknown' && $latest_version != 'unknown') {
+                    if ($installed_version < $latest_version) {
+                        exec("cd \"$plugin_dir\" && git checkout {$latest_version}", $messages);
+                    } else {
+                        $messages[] = "Already up-to-date.";
+                    }
+                } else {
+                    $messages[] = "Unable to update.  Semver compliance issue with tagged release.";
+                }
+                break;
+
+            case 'composer':
+                // Determine the project name.
+                $composer_info = $this->getComposerInfo($plugin);
+                $project = $composer_info['name'];
+                $packagist_url = "https://packagist.org/packages/{$project}";
+                if ($this->isValidUrl($packagist_url)) {
+                    // Get the Terminus major version.
+                    $terminus_major_version = $this->getTerminusMajorVersion();
+                    // Backup the plugin directory, just in case.
+                    $datetime = date('YmdHi', time());
+                    $backup_directory = $plugins_dir . '..' . DIRECTORY_SEPARATOR . 'backups' . DIRECTORY_SEPARATOR . 'plugins' . DIRECTORY_SEPARATOR . $project . DIRECTORY_SEPARATOR . $datetime;
+                    exec("mkdir -p {$backup_directory} && tar czvf {$backup_directory}" . DIRECTORY_SEPARATOR . "backup.tar.gz \"{$plugin_dir}\"", $backup_messages);
+                    // Create a new project via Composer.
+                    $composer_command = "composer create-project --prefer-source --keep-vcs -n -d {$plugins_dir} {$project}:~{$terminus_major_version}";
+                    exec("rm -rf \"{$plugin_dir}\" && {$composer_command}", $install_messages);
+                    $messages = array_merge($backup_messages, $install_messages);
+                    $messages[] = "Backed up the project to {$backup_directory}" . DIRECTORY_SEPARATOR . "backup.tar.gz.";
+                } else {
+                    $messages[] = "Unable to update.  {$packagist_url} is not a valid Packagist project.";
+                }
+                break;
+
+            default:
+                $messages[] = "Unable to update.  Plugin is not a valid Packagist project.";
+        }
+        foreach ($messages as $message) {
+            $this->log()->notice($message);
+        }
+    }
 }
