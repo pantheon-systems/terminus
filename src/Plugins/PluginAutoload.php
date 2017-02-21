ec057586903512d10f9716a21c06175b7917458f
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-02-21 11:16:55 -0800

    Add a composer.lock validator.

--- src/Plugins/PluginAutoload.php
+++ src/Plugins/PluginAutoload.php
@@ -48,7 +48,7 @@ class PluginAutoload implements InitializeHookInterface, LoggerAwareInterface
 
         // If the commandfile path is inside Terminus, then
         // the autoload file has already been loaded.
-        if ($this->pathInside($path, $terminusSrcDir)) {
+        if ($this->pathIsInside($path, $terminusSrcDir)) {
             $this->logger->debug(
                 'Plugin Autoload: %dir is a Terminus source file.',
                 ['dir' => $path]
@@ -105,11 +105,19 @@ class PluginAutoload implements InitializeHookInterface, LoggerAwareInterface
         return dirname($terminusClass->getFileName());
     }
 
-    protected function pathInside($path, $terminusSrcDir)
+    /**
+     * Return 'true' if $path is contained anywhere inside
+     * the provided $terminusSrcDir.
+     */
+    protected function pathIsInside($path, $terminusSrcDir)
     {
         return substr($path, 0, strlen($terminusSrcDir)) == $terminusSrcDir;
     }
 
+    /**
+     * Find the plugin's base directory -- the one that contains the
+     * composer.json file.
+     */
     protected function findPluginBaseDir($path)
     {
         // Walk up one directory. If we are already at the root,
@@ -133,24 +141,143 @@ class PluginAutoload implements InitializeHookInterface, LoggerAwareInterface
         return $this->findPluginBaseDir($checkDir);
     }
 
-    protected function validateComposerLock($pluginBaseDir, $terminusSrcDir)
-    {
-        // If there is no composer.lock file, that means that
-        // the plugin has autoload classes, but requires no dependencies.
-        if (!file_exists("$pluginBaseDir/composer.lock")) {
-            return;
-        }
-
-        // TODO: Load the composer.lock and analyze it against
-        // dirname($terminusSrcDir) . '/composer.lock'.
-        throw new TerminusException("Autoloading plugin dependencies is not supported yet.");
-    }
-
+    /**
+     * Return the path to the autoload file, relative to the
+     * provided path, if it exists.
+     */
     protected function checkAutoloadPath($path)
     {
         $autoloadFile = "$path/vendor/autoload.php";
         if (file_exists($autoloadFile)) {
             return $autoloadFile;
         }
+    }
+
+    /**
+     * Compare the contents of the composer.json and composer.lock files
+     * of the plugin being used, and see if anything contaied therein is
+     * incompatible with the projects loaded by Terminus itself.
+     */
+    protected function validateComposerLock($pluginBaseDir, $terminusSrcDir)
+    {
+        $pluginComposerJsonFile = $pluginBaseDir . '/composer.json';
+        $pluginComposerLockFile = $pluginBaseDir . '/composer.lock';
+        $pluginComposerJson = $this->loadJson($pluginComposerJsonFile);
+        $pluginComposerLock = $this->loadJson($pluginComposerLockFile);
+
+        // If there is no composer.lock file, that means that
+        // the plugin has autoload classes, but requires no dependencies.
+        // In this case, we know it is safe to load the autoload file.
+        if (empty($pluginComposerLock)) {
+            return;
+        }
+
+        $terminusComposerJsonFile = dirname($terminusSrcDir) . '/composer.json';
+        $terminusComposerLockFile = dirname($terminusSrcDir) . '/composer.lock';
+        $terminusComposerJson = $this->loadJson($terminusComposerJsonFile);
+        $terminusComposerLock = $this->loadJson($terminusComposerLockFile);
+
+        if (empty($terminusComposerJson) || empty($terminusComposerLock)) {
+            throw new TerminusException("Could not load Terminus composer data.");
+        }
+
+        // If the plugin contains a requirement for something that is part
+        // of Terminus' autoload file, reject the plugin. These should be
+        // fixed by the plugin author by removing .
+        $this->validatePluginDoesNotRequireTerminusDependencies($pluginComposerJsonFile, $terminusComposerLockFile);
+
+        // If the plugin's lock file contains a project that is also in
+        // Terminus' lock file, require them to be at exactly the same
+        // version number.
+        $this->validateLockFilesCompatible($pluginComposerJsonFile, $pluginComposerLockFile, $terminusComposerLockFile);
+    }
+
+    /**
+     * Ensure that the plugin does not directly 'require' any dependency
+     * that Terminus already provides. If it does, we will reject the
+     * plugin. The plugin author may remove this dependency from the
+     * plugin's composer.json file via 'composer remove'.
+     */
+    protected function validatePluginDoesNotRequireTerminusDependencies($pluginComposerJsonFile, $terminusComposerLockFile)
+    {
+        $pluginName = $pluginComposerJsonFile['name'];
+        $terminusPackages = $this->getLockFilePackages($terminusComposerLockFile);
+
+        // No requirements, no issue. This condition should never be
+        // true, though, for if it were, there would be no composer.lock.
+        if (!isset($pluginComposerJsonFile['require'])) {
+            return;
+        }
+        $pluginRequirements = $pluginComposerJsonFile['require'];
+        unset($pluginRequirements['php']);
+
+        foreach ($pluginRequirements as $project => $versionConstraints) {
+            if (array_key_exists($project, $terminusPackages)) {
+                throw new TerminusException("The plugin {name} requires the project {dependency}, which is already provided by Terminus. Please remove this dependency from the plugin by running 'composer remove {dependency}' in the {name} plugin directory.", ['name' => $pluginName, 'dependency' => $project]);
+            }
+        }
+    }
+
+    /**
+     * Ensure that either:
+     *  a) nothing in the plugin composer.lock exists in Terminus's composer.lock (ideal)
+     * or
+     *  b) anything that does appear in both places exists as exactly the same version.
+     */
+    protected function validateLockFilesCompatible($pluginComposerJsonFile, $pluginComposerLockFile, $terminusComposerLockFile)
+    {
+        $pluginName = $pluginComposerJsonFile['name'];
+        $pluginPackages = $this->getLockFilePackages($pluginComposerLockFile);
+        $terminusPackages = $this->getLockFilePackages($terminusComposerLockFile);
+
+        foreach ($pluginPackages as $project => $version) {
+            if (array_key_exists($project, $terminusPackages)) {
+                if ($version != $terminusPackages[$project]) {
+                    throw new TerminusException("The plugin {name} has installed the project {dependency}: {version}, but Terminus has installed {dependency}: {otherversion}. To resolve this, try running 'composer update' in both the plugin directory, and the terminus directory.", ['name' => $pluginName, 'dependency' => $project, 'version' => $version, 'otherversion' => $terminusPackages[$project]]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Look through the composer.lock file and gather up all of the
+     * projects contained therein. Return them as a simple associative
+     * array of name => version mappings.
+     */
+    protected function getLockFilePackages($lockFile)
+    {
+        $lockFile += ['packages' => [], 'packages-dev' => []];
+
+        return collectLockFilePackages($lockFile['packages']) + collectLockFilePackages($lockFile['packages-dev']);
+    }
+
+    /**
+     * Like 'getLockFilePackages', but operates on just one
+     * packages section.
+     */
+    protected function collectLockFilePackages($packages)
+    {
+        $result;
+
+        foreach ($packages as $package) {
+            $result[$packages['name']] = $packages['version'];
+        }
+
+        return $result;
+    }
+
+    /**
+     * Read the contents of a file and convert to a json array.
+     */
+    protected function loadJson($pathToJson)
+    {
+        if (!file_exists($pathToJson)) {
+            return [];
+        }
+        $contents = file_get_contents($pathToJson);
+        if (empty($contents)) {
+            return [];
+        }
+        return json_decode($contents, true);
     }
 }
