eb927aa15cc8504224c48adc622cef0abd74a128
tesladethray <tesladethray@gmail.com>
2017-05-25 21:07:02 -0700

    Added tests for Pantheon\Terminus\Models\WorkflowOperation

--- tests/unit_tests/Models/WorkflowOperationTest.php
+++ tests/unit_tests/Models/WorkflowOperationTest.php
@@ -11,13 +11,21 @@ use Pantheon\Terminus\Models\WorkflowOperation;
  */
 class WorkflowOperationTest extends ModelTestCase
 {
+    /**
+     * Tests the WorkflowOperation::description() function
+     */
     public function testDescription()
     {
-        $wfop = new WorkflowOperation((object)['description' => 'Dumbo Drop', 'run_time' => 1.2345]);
+        $wfop = new WorkflowOperation((object)['description' => 'Dumbo Drop', 'run_time' => 1.2345,]);
+        $this->assertEquals('Operation: Dumbo Drop finished in 1s', $wfop->description());
 
-        $this->assertEquals("Operation: Dumbo Drop finished in 1s", $wfop->description());
+        $wfop2 = new WorkflowOperation((object)['description' => 'Dumbo Drop',]);
+        $this->assertEquals('Operation: Dumbo Drop finished in ', $wfop2->description());
     }
 
+    /**
+     * Tests the WorkflowOperation::serialize() function
+     */
     public function testSerialize()
     {
         $data = [
@@ -35,5 +43,24 @@ class WorkflowOperationTest extends ModelTestCase
         unset($data['other']);
         unset($data['run_time']);
         $this->assertEquals($data, $wfop->serialize());
+    }
+
+    /**
+     * Tests the WorflowOperation::__toString() function
+     */
+    public function testToString()
+    {
+        $data = [
+            'description' => 'Dumbo Drop',
+            'run_time' => 1.2345,
+            'log_output' => 'The operation was a total success',
+            'environment' => 'multidev',
+        ];
+        $wfop = new WorkflowOperation((object)$data);
+
+        $this->assertEquals(
+            "------ Operation: Dumbo Drop finished in 1s (multidev) ------\nThe operation was a total success",
+            (string)$wfop
+        );
     }
 }
