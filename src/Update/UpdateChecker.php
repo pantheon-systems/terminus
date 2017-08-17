c455e8806646a9c5f2388bf95826d3f7bda62734
tesladethray <tesladethray@gmail.com>
2017-08-17 10:12:03 -0700

    Updated dependencies

--- src/Update/UpdateChecker.php
+++ src/Update/UpdateChecker.php
@@ -17,7 +17,11 @@ use Robo\Contract\ConfigAwareInterface;
  * Class UpdateChecker
  * @package Pantheon\Terminus\Update
  */
-class UpdateChecker implements ConfigAwareInterface, ContainerAwareInterface, DataStoreAwareInterface, LoggerAwareInterface
+class UpdateChecker implements
+    ConfigAwareInterface,
+    ContainerAwareInterface,
+    DataStoreAwareInterface,
+    LoggerAwareInterface
 {
     use ConfigAwareTrait;
     use ContainerAwareTrait;
@@ -25,7 +29,9 @@ class UpdateChecker implements ConfigAwareInterface, ContainerAwareInterface, Da
     use LoggerAwareTrait;
 
     const DEFAULT_COLOR = "\e[0m";
-    const UPDATE_COMMAND = 'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar && php installer.phar update';
+    const INSTALLER_COMMAND =
+        'curl -O https://raw.githubusercontent.com/pantheon-systems/terminus-installer/master/builds/installer.phar';
+    const UPDATE_COMMAND = 'php installer.phar update';
     const UPDATE_NOTICE = <<<EOT
 A new Terminus version v{latest_version} is available.
 You are currently using version v{running_version}. 
