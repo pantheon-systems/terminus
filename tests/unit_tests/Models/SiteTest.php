2d410e7f3f40f4e7b5f85ddbe61758e6ccb34f0c
Sara McCutcheon <TeslaDethray@users.noreply.github.com>
2017-11-03 09:23:45 -0700

    Added last_frozen_at to output of site:info (#1788)

--- tests/unit_tests/Models/SiteTest.php
+++ tests/unit_tests/Models/SiteTest.php
@@ -485,6 +485,7 @@ class SiteTest extends ModelTestCase
             'holder_id' => 'holder id',
             'owner' => 'owner id',
             'frozen' => 'yes',
+            'last_frozen_at' => '1682641540',
         ];
         $expected_data = [
             'id' => $this->model->id,
@@ -502,7 +503,8 @@ class SiteTest extends ModelTestCase
             'frozen' => 'true',
             'memberships' => implode(',', $this->model->memberships),
             'tags' => implode(',', $tags),
-            'max_num_cdes' => null,
+            'max_num_cdes' => 0,
+            'last_frozen_at' => '2023-04-28 00:25:40',
         ];
 
         $this->request->expects($this->once())
@@ -543,6 +545,7 @@ class SiteTest extends ModelTestCase
             'holder_id' => 'holder id',
             'owner' => 'owner id',
             'frozen' => 'yes',
+            'last_frozen_at' => 'April 28, 2023 9:20PM',
         ];
         $expected_data = [
             'id' => $this->model->id,
@@ -560,7 +563,8 @@ class SiteTest extends ModelTestCase
             'frozen' => 'true',
             'memberships' => implode(',', $this->model->memberships),
             'tags' => implode(',', $tags),
-            'max_num_cdes' => null,
+            'max_num_cdes' => 0,
+            'last_frozen_at' => '2023-04-28 21:20:00',
         ];
 
         $this->request->expects($this->once())
