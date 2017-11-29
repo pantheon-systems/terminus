267a25aa6dc8226448aced8f8510d0fcec7f7b81
Greg Anderson <greg.1.anderson@greenknowe.org>
2017-11-29 15:09:49 -0800

    Remove custom progress bar formatting. (#1801)

--- src/Style/TerminusStyle.php
+++ src/Style/TerminusStyle.php
@@ -13,17 +13,4 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  */
 class TerminusStyle extends SymfonyStyle
 {
-    const NORMAL_PROGRESS_FORMAT = " <bg=blue;fg=black> %message% </>\n <info>Progress: %bar% <info>%percent:3s%%</info> \n <info>Elapsed: %elapsed:6s% \n Estimated Time Remaining: %estimated:-6s%</info>";
-
-    /**
-     * TerminusStyle constructor.
-     *
-     * @param \Symfony\Component\Console\Input\InputInterface $input
-     * @param \Symfony\Component\Console\Output\OutputInterface $output
-     */
-    public function __construct(InputInterface $input, OutputInterface $output)
-    {
-        ProgressBar::setFormatDefinition('normal', self::NORMAL_PROGRESS_FORMAT);
-        parent::__construct($input, $output);
-    }
 }
