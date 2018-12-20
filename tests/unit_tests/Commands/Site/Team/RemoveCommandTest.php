adc4d232b559b7cf2d3232bc1d290268d71720e7
Sara McCutcheon <TeslaDethray@users.noreply.github.com>
2018-12-20 10:04:16 -0800

    Added try-catch block to site:team:remove to prevent the success of the command when removing oneself from resulting in an error (#1915)

--- tests/unit_tests/Commands/Site/Team/RemoveCommandTest.php
+++ tests/unit_tests/Commands/Site/Team/RemoveCommandTest.php
@@ -2,6 +2,9 @@
 
 namespace Pantheon\Terminus\UnitTests\Commands\Site\Team;
 
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
 use Pantheon\Terminus\Commands\Site\Team\RemoveCommand;
 use Pantheon\Terminus\UnitTests\Commands\WorkflowProgressTrait;
 
@@ -15,11 +18,23 @@ class RemoveCommandTest extends TeamCommandTest
     use WorkflowProgressTrait;
 
     /**
+     * @var string
+     */
+    protected $message;
+
+    /**
      * Setup the test fixture.
      */
     protected function setUp()
     {
         parent::setUp();
+
+        $this->message = 'message';
+
+        $this->user_membership->expects($this->once())
+            ->method('delete')
+            ->willReturn($this->workflow);
+
         $this->command = new RemoveCommand($this->getConfig());
         $this->command->setLogger($this->logger);
         $this->command->setSites($this->sites);
@@ -27,25 +42,55 @@ class RemoveCommandTest extends TeamCommandTest
     }
 
     /**
-     * Tests the site:team:remove command
+     * Tests the site:team:remove command when it succeeds without issue
      */
     public function testRemoveCommand()
     {
-        $message = 'message';
-
-        $this->user_membership->expects($this->once())
-            ->method('delete')
-            ->willReturn($this->workflow);
         $this->workflow->expects($this->once())
             ->method('getMessage')
             ->with()
-            ->willReturn($message);
+            ->willReturn($this->message);
         $this->logger->expects($this->once())
             ->method('log')
             ->with(
                 $this->equalTo('notice'),
-                $this->equalTo($message)
+                $this->equalTo($this->message)
             );
+
+        $out = $this->command->remove('mysite', 'test@example.com');
+        $this->assertNull($out);
+    }
+
+    /**
+     * Tests the site:team:remove command when the workflow throws an error because
+     * the user is no longer a team member permitted to access the site's workflows
+     */
+    public function testRemoveCommandRemovingSelf()
+    {
+        $this->progress_bar->method('cycle')
+            ->with()
+            ->will($this->throwException(new \Exception($this->message, 404)));
+        $this->logger->expects($this->once())
+            ->method('log')
+            ->with(
+                $this->equalTo('notice'),
+                $this->equalTo('Removed your user from site team')
+            );
+
+        $out = $this->command->remove('mysite', 'test@example.com');
+        $this->assertNull($out);
+    }
+
+    /**
+     * Tests the site:team:remove command when the workflow throws an error but it is not the error
+     * which would occur when removing oneself from a team
+     */
+    public function testRemoveCommandErrs()
+    {
+        $this->progress_bar->method('cycle')
+            ->with()
+            ->will($this->throwException(new \Exception($this->message, 403)));
+        $this->setExpectedException(\Exception::class, $this->message);
 
         $out = $this->command->remove('mysite', 'test@example.com');
         $this->assertNull($out);
