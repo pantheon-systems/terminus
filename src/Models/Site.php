8c1b2bb12522a819f5027f4a0d7bb3ecb8332298
AlexD <alex@getpantheon.com>
2019-06-11 15:07:27 +0300

    Use region human readable name instead of GCP alias

--- src/Models/Site.php
+++ src/Models/Site.php
@@ -383,7 +383,7 @@ class Site extends TerminusModel implements ContainerAwareInterface, Organizatio
             'holder_type' => $this->get('holder_type'),
             'holder_id' => $this->get('holder_id'),
             'owner' => $this->get('owner'),
-            'region' => $this->get('preferred_zone'),
+            'region' => $this->get('preferred_zone_label'),
             'frozen' => $this->isFrozen(),
             'last_frozen_at' => $this->get('last_frozen_at'),
         ];
