2635f796c5f57a4c331444b6b44d49e16a74ecb8
Dave Reid <dave@davereid.net>
2019-07-18 12:49:55 -0500

    Fixed coding standard.

--- src/Commands/Env/CloneContentCommand.php
+++ src/Commands/Env/CloneContentCommand.php
@@ -70,8 +70,8 @@ class CloneContentCommand extends TerminusCommand implements SiteAwareInterface
         $this->target_env = $site->getEnvironments()->get($target_env);
 
         if ($this->source_env->id === $target_env) {
-          $this->log()->notice('Skipping clone since environments are the same.');
-          return;
+            $this->log()->notice('Skipping clone since environments are the same.');
+            return;
         }
 
         $this->checkForInitialization($this->source_env, 'from');
