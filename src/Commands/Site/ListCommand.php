7244e4b5a265d059f132694f986bce68af2c3d48
Oubah Awaleh <oubawaleh@gmail.com>
2019-07-18 15:11:09 -0700

    Added filter option

--- src/Commands/Site/ListCommand.php
+++ src/Commands/Site/ListCommand.php
@@ -32,10 +32,10 @@ class ListCommand extends SiteCommand
      * @default-fields name,id,plan_name,framework,region,owner,created,memberships,frozen
      * @return RowsOfFields
      *
-     * @option name Name filter
-     * @option org Organization filter; "all" or an organization's name, label, or ID
+     * @option name DEPRECATED Name filter
+     * @option org DEPRECATED Organization filter; "all" or an organization's name, label, or ID
      * @option owner Owner filter; "me" or user UUID
-     * @option plan Plan filter; filter by the plan's label
+     * @option plan DEPRECATED Plan filter; filter by the plan's label
      * @option team Team-only filter
      * @option string $upstream Upstream name to filter
      *
