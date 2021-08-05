b6c0ed5bf9cd10e7b1387ebd5165d874f4a4b5af
Kevin Porras <kporras07@gmail.com>
2021-08-04 19:05:54 -0600

    Throw exception if variable is different than array or string.

--- src/Helpers/LocalMachineHelper.php
+++ src/Helpers/LocalMachineHelper.php
@@ -199,7 +199,7 @@ class LocalMachineHelper implements ConfigAwareInterface, ContainerAwareInterfac
     /**
      * Returns a set-up process object.
      *
-     * @param string $cmd The command to execute
+     * @param string|array $cmd The command to execute
      * @return Process
      */
     protected function getProcess($cmd)
@@ -210,10 +210,11 @@ class LocalMachineHelper implements ConfigAwareInterface, ContainerAwareInterfac
         } elseif (is_array($cmd)) {
             $process = new Process($cmd);
         }
-        if ($process) {
-            $config = $this->getConfig();
-            $process->setTimeout($config->get('timeout'));
+        if (!$process) {
+            throw new \InvalidArgumentException('$cmd should be a string or an array.');
         }
+        $config = $this->getConfig();
+        $process->setTimeout($config->get('timeout'));
         return $process;
     }
 }
