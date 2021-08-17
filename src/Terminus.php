0bf2d0ae49f2c075a93277f20f2d86e4d04d47c9
Kevin Porras <kporras07@gmail.com>
2021-08-16 20:30:14 -0600

    Use terminus dependencies base dir to improve testability.

--- src/Terminus.php
+++ src/Terminus.php
@@ -504,7 +504,7 @@ class Terminus implements
 
 
 
-    public static function factory($terminusDependenciesDir): Terminus
+    public static function factory($terminusDependenciesVersion): Terminus
     {
         $input = new ArgvInput($_SERVER['argv']);
         $output = new ConsoleOutput();
@@ -513,7 +513,12 @@ class Terminus implements
         $config->extend(new YamlConfig($config->get('user_home') . '/.terminus/config.yml'));
         $config->extend(new DotEnvConfig(getcwd()));
         $config->extend(new EnvConfig());
+        $dependenciesBaseDir = $config->get('dependencies_base_dir');
+        $terminusDependenciesDir = $dependenciesBaseDir . '/' . $terminusDependenciesVersion;
         $config->set('terminus_dependencies_dir', $terminusDependenciesDir);
+        if (file_exists($terminusDependenciesDir . '/vendor/autoload.php')) {
+            include_once("$terminusDependenciesDir/vendor/autoload.php");
+        }
         return new static($config, $input, $output);
     }
 }
