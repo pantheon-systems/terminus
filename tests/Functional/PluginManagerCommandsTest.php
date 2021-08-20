da76bcb6bce9ed6ceb6f907ce63debfce88c9f4b
Kevin Porras <kporras07@gmail.com>
2021-08-19 20:27:47 -0600

    Add initial tests for plugin manager.

---
+++ tests/Functional/PluginManagerCommandsTest.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Pantheon\Terminus\Tests\Functional;
+
+use Pantheon\Terminus\Tests\Traits\TerminusTestTrait;
+use Pantheon\Terminus\Tests\Traits\UrlStatusCodeHelperTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class DomainCommandsTest
+ *
+ * @package Pantheon\Terminus\Tests\Functional
+ */
+class PluginManagerCommandsTest extends TestCase
+{
+    use TerminusTestTrait;
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Self\ListCommand
+     * @covers \Pantheon\Terminus\Commands\Self\ReloadCommand
+     * @covers \Pantheon\Terminus\Commands\Self\InstallCommand
+     * @covers \Pantheon\Terminus\Commands\Self\UpdateCommand
+     * @covers \Pantheon\Terminus\Commands\Self\UninstallCommand
+     * @covers \Pantheon\Terminus\Commands\Self\SearchCommand
+     *
+     * @group plugins
+     * @group long
+     */
+    public function testPluginsOperations()
+    {
+        $pluginsDir = $this->getPluginsDir();
+        $dependenciesBaseDir = $this->getDependenciesBaseDir();
+        $this->removeDir($pluginsDir);
+        $this->removeDir($dependenciesBaseDir);
+
+        // LIST COMMANDS TO CHECK THAT PLUGIN COMMANDS ARE NOT AVAILABLE
+        $command = $this->getTerminusPluginTestCommand();
+        $this->terminus("list | grep $command", 1);
+
+        // LIST PLUGINS
+        $results = $this->terminus("self:plugin:list 2>&1");
+        $this->assertStringContainsString("You have no plugins installed", $results, "Terminus plugins should be empty at this point.");
+        
+        // ADD PLUGIN
+        $plugin = $this->getTerminusPluginToInstall();
+        $results = $this->terminus("self:plugin:install $plugin 2>&1");
+        $this->assertStringContainsString("Installed $plugin", $results, "Terminus plugin installation failed.");
+
+        // LIST PLUGINS AGAIN
+        $results = $this->terminusJsonResponse("self:plugin:list");
+        $this->assertIsArray($results, "Returned values from self:plugin:list should be array");
+        $this->assertGreaterThan(
+            0,
+            count($results),
+            "Count of plugins should be greater than 0"
+        );
+        $this->assertStringContainsString($this->getTerminusPluginInstalled(), $results[0]['name'], "Terminus plugin recently installed is not listed.");
+        
+        // LIST COMMANDS AGAIN
+        $this->terminus("list | grep $command");
+
+        // TRY UPDATING PLUGIN
+        $plugin = $this->getTerminusPluginToUpdate();
+        $results = $this->terminus("self:plugin:update $plugin 2>&1");
+        $this->assertStringContainsString("Nothing to install, update or remove", $results, "Terminus plugin update failed.");
+
+        // LIST PLUGINS AGAIN
+        $results = $this->terminusJsonResponse("self:plugin:list");
+        $this->assertIsArray($results, "Returned values from self:plugin:list should be array");
+        $this->assertGreaterThan(
+            0,
+            count($results),
+            "Count of plugins should be greater than 0"
+        );
+        $this->assertStringContainsString($this->getTerminusPluginInstalled(), $results[0]['name'], "Terminus plugin recently installed is not listed.");
+
+        // LIST COMMANDS AGAIN
+        $this->terminus("list | grep $command");
+
+        // TRY RELOADING PLUGINS
+        $results = $this->terminus("self:plugin:reload 2>&1");
+        $this->assertStringContainsString("Plugins reload done", $results, "Terminus plugin reload failed.");
+
+        // LIST PLUGINS AGAIN
+        $results = $this->terminusJsonResponse("self:plugin:list");
+        $this->assertIsArray($results, "Returned values from self:plugin:list should be array");
+        $this->assertGreaterThan(
+            0,
+            count($results),
+            "Count of plugins should be greater than 0"
+        );
+        $this->assertStringContainsString($this->getTerminusPluginInstalled(), $results[0]['name'], "Terminus plugin recently installed is not listed.");
+
+        // LIST COMMANDS AGAIN
+        $this->terminus("list | grep $command");
+
+        // TRY UNINSTALLING PLUGIN
+        $plugin = $this->getTerminusPluginToUninstall();
+        $results = $this->terminus("self:plugin:uninstall $plugin 2>&1");
+        $this->assertStringContainsString("Uninstalled $plugin", $results, "Terminus plugin uninstall failed.");
+
+        // LIST PLUGINS
+        $results = $this->terminus("self:plugin:list 2>&1");
+        $this->assertStringContainsString("You have no plugins installed", $results, "Terminus plugins should be empty at this point.");
+
+        // LIST COMMANDS AGAIN TO CHECK THAT PLUGIN COMMANDS ARE NOT AVAILABLE
+        $this->terminus("list | grep $command", 1);
+
+        // @todo Kevin search.
+    }
+
+    /**
+     * Returns the terminus plugin to install.
+     *
+     * @return string
+     */
+    protected function getTerminusPluginToInstall(): string
+    {
+        return getenv('TERMINUS_PLUGIN_TO_INSTALL');
+    }
+
+    /**
+     * Returns the terminus plugin that should have been installed.
+     *
+     * @return string
+     */
+    protected function getTerminusPluginInstalled(): string
+    {
+        return getenv('TERMINUS_PLUGIN_INSTALLED');
+    }
+
+    /**
+     * Returns terminus command that should now be available.
+     *
+     * @return string
+     */
+    protected function getTerminusPluginTestCommand(): string
+    {
+        return getenv('TERMINUS_PLUGIN_TEST_COMMAND');
+    }
+
+    /**
+     * Returns the terminus plugin to update.
+     *
+     * @return string
+     */
+    protected function getTerminusPluginToUpdate(): string
+    {
+        return getenv('TERMINUS_PLUGIN_TO_UPDATE_UNINSTALL');
+    }
+
+    /**
+     * Returns the terminus plugin to uninstall.
+     *
+     * @return string
+     */
+    protected function getTerminusPluginToUninstall(): string
+    {
+        return getenv('TERMINUS_PLUGIN_TO_UPDATE_UNINSTALL');
+    }
+}
--- tests/Traits/TerminusTestTrait.php
+++ tests/Traits/TerminusTestTrait.php
@@ -14,7 +14,7 @@ trait TerminusTestTrait
      *
      * @param string $command The command to run
      *
-     * @reutrn array
+     * @return array
      *   The execution's output and status.
      */
     protected static function callTerminus(string $command): array
@@ -82,5 +82,49 @@ trait TerminusTestTrait
     protected function getSiteName(): string
     {
         return getenv('TERMINUS_SITE');
+    }
+
+    /**
+     * Returns the plugin dir.
+     *
+     * @return string
+     */
+    protected function getPluginsDir(): string
+    {
+        return getenv('TERMINUS_PLUGINS_DIR');
+    }
+
+    /**
+     * Returns the dependencies base dir.
+     *
+     * @return string
+     */
+    protected function getDependenciesBaseDir(): string
+    {
+        return getenv('TERMINUS_DEPENDENCIES_BASE_DIR');
+    }
+
+    /**
+     * Remove given dir.
+     *
+     * @param string $dir The dir to remove
+     *
+     * @return array
+     *   The execution's output and status.
+     */
+    protected static function removeDir(string $dir): array
+    {
+        if (is_dir($dir)) {
+            $project_dir = dirname(dirname(__DIR__));
+            exec(
+                sprintf("rm -r %s", $dir),
+                $output,
+                $status
+            );
+            $output = implode("\n", $output);
+
+            return [$output, $status];
+        }
+        return ['', 0];
     }
 }
