85aad3d5d7a55e19fd25567e267d6efd73d21a20
Sergei Churilo <sergei.churilo@pantheon.io>
2021-09-01 14:16:51 +0300

    Add testWorkflowOperationsAndLogsCommands()

--- tests/Functional/WorkflowCommandsTest.php
+++ tests/Functional/WorkflowCommandsTest.php
@@ -27,12 +27,104 @@ class WorkflowCommandsTest extends TestCase
      */
     public function testWorkflowListAndStatusCommand()
     {
+        $workflow = $this->getLatestWorkflow();
+        $workflowStatus = $this->terminusJsonResponse(
+            sprintf('workflow:info:status %s --id=%s', $this->getSiteName(), $workflow['id'])
+        );
+
+        unset($workflowStatus['time']);
+        unset($workflow['time']);
+        $this->assertEquals($workflowStatus, $workflow);
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Workflow\ListCommand
+     * @covers \Pantheon\Terminus\Commands\Workflow\Info\OperationsCommand
+     * @covers \Pantheon\Terminus\Commands\Workflow\Info\LogsCommand
+     *
+     * @group workflow
+     * @group short
+     *
+     * Requirements:
+     * 1. Test site has print-test-message.php script file in web/private/scripts/quicksilver/ path on dev environment
+     *    with the following lines:
+     *    <code>
+     *    <?php
+     *    print 'This message should be printed after env:clear-cache Terminus command execution.';
+     *    </code>
+     *  2. Test site has the following quicksilver config in pantheon.yml file on dev environment:
+     *    <code>
+     *    workflows:
+     *      after:
+     *       - type: webphp
+     *         description: Print test message
+     *         script: private/scripts/quicksilver/print-test-message.php
+     *    </code>
+     */
+    public function testWorkflowOperationsAndLogsCommands()
+    {
+        $env = 'dev';
+        $this->terminus(sprintf('env:clear-cache %s.%s', $this->getSiteName(), $env));
+
+        $workflow = $this->getLatestWorkflow(['workflow' => sprintf('Clear cache for "%s"', $env)]);
+        $this->assertEquals('succeeded', $workflow['status']);
+        $this->assertEquals($env, $workflow['env']);
+        $this->assertEquals(sprintf('Clear cache for "%s"', $env), $workflow['workflow']);
+
+        $operations = $this->terminusJsonResponse(
+            sprintf('workflow:info:operations %s --id=%s', $this->getSiteName(), $workflow['id'])
+        );
+        $this->assertIsArray($operations);
+        $this->assertNotEmpty($operations);
+
+        $testOperation = array_pop($operations);
+        $this->assertIsArray($testOperation);
+        $this->assertNotEmpty($testOperation);
+        $this->assertArrayHasKey('type', $testOperation);
+        $this->assertEquals('quicksilver', $testOperation['type']);
+        $this->assertArrayHasKey('result', $testOperation);
+        $this->assertEquals('succeeded', $testOperation['result']);
+        $this->assertArrayHasKey('duration', $testOperation);
+        $this->assertArrayHasKey('description', $testOperation);
+        $this->assertEquals('Print test message', $testOperation['description']);
+
+        $logs = $this->terminus(
+            sprintf('workflow:info:logs %s --id=%s', $this->getSiteName(), $workflow['id'])
+        );
+        $this->assertIsString($logs);
+        $this->assertNotEmpty($logs);
+
+        $this->assertTrue(
+            false !== strpos($logs, 'This message should be printed after env:clear-cache Terminus command execution.'),
+            'Workflow log should contain the test message'
+        );
+    }
+
+    /**
+     * Tests and returns the latest workflow record.
+     *
+     * @param array $filters
+     *   The list of filters (field name => expected value).
+     *
+     * @return array
+     *   The workflow metadata.
+     */
+    private function getLatestWorkflow(array $filters = []): array
+    {
         $workflowsList = $this->terminusJsonResponse(sprintf('workflow:list %s', $this->getSiteName()));
         $this->assertIsArray($workflowsList);
         $this->assertNotEmpty($workflowsList);
 
-        $workflowUuid = array_key_first($workflowsList);
+        if ($filters) {
+            $workflowsList = array_filter(
+                $workflowsList,
+                fn($workflow): bool => count(array_intersect_assoc($filters, $workflow)) === count($filters)
+            );
+        }
+
         $workflow = array_shift($workflowsList);
+
         $fields = [
             'id',
             'env',
@@ -47,29 +139,10 @@ class WorkflowCommandsTest extends TestCase
             $this->assertArrayHasKey(
                 $field,
                 $workflow,
-                sprintf('Workflow record should contain "%s" field. %s', $field, print_r($workflow, true))
+                sprintf('Workflow should have "%s" field', $field)
             );
         }
 
-        $workflowStatus = $this->terminusJsonResponse(
-            sprintf('workflow:info:status %s --id=%s', $this->getSiteName(), $workflowUuid)
-        );
-        unset($workflowStatus['time']);
-        unset($workflow['time']);
-        $this->assertEquals($workflowStatus, $workflow);
-    }
-
-    /**
-     * @test
-     * @covers \Pantheon\Terminus\Commands\Workflow\WatchCommand
-     * @covers \Pantheon\Terminus\Commands\Workflow\Info\OperationsCommand
-     * @covers \Pantheon\Terminus\Commands\Workflow\Info\InfoBaseCommand
-     *
-     * @group workflow
-     * @group todo
-     */
-    public function testWorkflowCommands()
-    {
-        $this->fail('To Be Written.');
+        return $workflow;
     }
 }
