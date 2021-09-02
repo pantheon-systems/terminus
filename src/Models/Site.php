7363353cbe5c5439f7fbed3a58f9d528b3919a71
Sergei Churilo <sergei.churilo@pantheon.io>
2021-09-02 11:38:55 +0300

    Fix plan:info command

--- src/Models/Site.php
+++ src/Models/Site.php
@@ -270,7 +270,7 @@ class Site extends TerminusModel implements ContainerAwareInterface, Organizatio
         if (empty($this->plan)) {
             $nickname = \uniqid(__FUNCTION__ . "-");
             $this->getContainer()->add($nickname, Plan::class)
-                ->addArgument(['site' => $this]);
+                ->addArguments([null, ['site' => $this]]);
             $this->plan = $this->getContainer()->get($nickname);
         }
         return $this->plan;
