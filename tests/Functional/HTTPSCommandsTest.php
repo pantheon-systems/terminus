8a515e677b967e65c66413a05f659e790f400c11
Sergei Churilo <sergei.churilo@pantheon.io>
2021-09-07 10:33:52 +0300

    Add testHttpsInfoCommand() (#2192)

--- tests/Functional/HTTPSCommandsTest.php
+++ tests/Functional/HTTPSCommandsTest.php
@@ -19,14 +19,31 @@ class HTTPSCommandsTest extends TestCase
     /**
      * @test
      * @covers \Pantheon\Terminus\Commands\HTTPS\InfoCommand
-     * @covers \Pantheon\Terminus\Commands\HTTPS\RemoveCommand
-     * @covers \Pantheon\Terminus\Commands\HTTPS\SetCommand
      *
      * @group https
-     * @group todo
+     * @group short
      */
-    public function testHTTPS()
+    public function testHttpsInfoCommand()
     {
-        $this->fail("To Be Written");
+        $httpsInfo = $this->terminusJsonResponse(sprintf('https:info %s.%s', $this->getSiteName(), 'dev'));
+        $this->assertIsArray($httpsInfo);
+        $this->assertNotEmpty($httpsInfo);
+        $key = sprintf('%s-%s.pantheonsite.io', 'dev', $this->getSiteName());
+        $this->assertArrayHasKey($key, $httpsInfo);
+
+        $this->assertArrayHasKey('id', $httpsInfo[$key]);
+        $this->assertEquals($key, $httpsInfo[$key]['id']);
+
+        $this->assertArrayHasKey('type', $httpsInfo[$key]);
+        $this->assertEquals('platform', $httpsInfo[$key]['type']);
+
+        $this->assertArrayHasKey('status', $httpsInfo[$key]);
+        $this->assertEquals('OK', $httpsInfo[$key]['status']);
+
+        $this->assertArrayHasKey('status_message', $httpsInfo[$key]);
+        $this->assertEquals('Launched', $httpsInfo[$key]['status_message']);
+
+        $this->assertArrayHasKey('deletable', $httpsInfo[$key]);
+        $this->assertFalse($httpsInfo[$key]['deletable']);
     }
 }
