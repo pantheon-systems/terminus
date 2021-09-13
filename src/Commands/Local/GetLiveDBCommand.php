6ca7eb732141c027c18723bb7526afe9ad59fbfc
Sergei Churilo <sergei.churilo@pantheon.io>
2021-09-13 12:43:54 +0300

    Refactor GetLiveDBCommand.php

--- src/Commands/Local/GetLiveDBCommand.php
+++ src/Commands/Local/GetLiveDBCommand.php
@@ -6,14 +6,12 @@ use Pantheon\Terminus\Commands\TerminusCommand;
 use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
 use Pantheon\Terminus\Exceptions\TerminusProcessException;
 use Pantheon\Terminus\Friends\LocalCopiesTrait;
-use Pantheon\Terminus\Models\Environment;
 use Pantheon\Terminus\Models\Site;
 use Pantheon\Terminus\Models\Workflow;
 use Pantheon\Terminus\Request\RequestAwareInterface;
 use Pantheon\Terminus\Request\RequestAwareTrait;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
-use Pantheon\Terminus\Exceptions\TerminusException;
 
 /**
  * Class GetLiveDBCommand.
@@ -37,7 +35,7 @@ class GetLiveDBCommand extends TerminusCommand implements
      * @command local:getLiveDB
      * @aliases ldb
      *
-     * @param string $site Site
+     * @param string|Site $site Site
      * @option bool $overwrite Overwrite existing file
      *
      * @usage <site> Create new backup of your live site and download to $HOME/pantheon-local-copies/{Site}/db
@@ -48,47 +46,23 @@ class GetLiveDBCommand extends TerminusCommand implements
      */
     public function downloadLiveDbBackup($site, $options = ['overwrite' => false])
     {
-        $siteData = $site;
-        if (!$siteData instanceof Site) {
-            $siteData = $this->getSite($site);
-            if (!$siteData instanceof Site) {
-                throw new TerminusException(
-                    'Cannot find site with the ID: {site}',
-                    ['site' => $site]
-                );
-            }
-        }
-        $liveEnv = $siteData
-            ->getEnvironments()
-            ->get('live');
-        $db_folder = $this->getLocalCopiesFolder() . DIRECTORY_SEPARATOR . 'db';
-        $db_local_filename =  sprintf(
-            '%s%s%s-db.tgz',
-            $db_folder,
-            DIRECTORY_SEPARATOR,
-            $siteData->getName()
-        );
-        if (!is_dir($db_folder)) {
-            mkdir($db_folder);
-            if (!is_dir($db_folder)) {
-                throw new TerminusException(
-                    'Cannot create {path}:',
-                    ['path' => $db_folder]
-                );
-            }
+        if (!$site instanceof Site) {
+            $site = $this->getSite($site);
         }
 
-        if (!$liveEnv instanceof Environment) {
-            throw new TerminusException("Cannot locate site's Live Environment.");
-        }
+        /** @var \Pantheon\Terminus\Models\Environment $liveEnv */
+        $liveEnv = $site
+            ->getEnvironments()
+            ->get('live');
+
         $this->logger->notice(
             '===> Fetching the backup catalog for {site}.',
             ['site' => $liveEnv->getName()]
         );
         $backups = $liveEnv->getBackups();
         $backups->fetch();
-        $db_backups = $backups->getBackupsByElement('database');
-        if (count($db_backups) === 0) {
+        $dbBackups = $backups->getBackupsByElement('database');
+        if (count($dbBackups) === 0) {
             $this->logger->notice(
                 '===> Creating database backup for {site}',
                 ['site' => $liveEnv->getName()]
@@ -97,24 +71,31 @@ class GetLiveDBCommand extends TerminusCommand implements
             if ($backupWorkflow instanceof Workflow) {
                 $this->processWorkflow($backupWorkflow);
                 if (!$backupWorkflow->isSuccessful()) {
-                    throw new TerminusProcessException('Backup Workflow Failed.');
+                    throw new TerminusProcessException('Backup workflow failed.');
                 }
             }
             $backups->fetch();
-            $db_backups = $backups->getBackupsByElement('database');
+            $dbBackups = $backups->getBackupsByElement('database');
         }
-        $lastBackup = reset($db_backups);
+        $lastBackup = reset($dbBackups);
         $this->logger->notice(
-            '===> Downloading db backup of {site} to {folder}.',
-            ['site' => $liveEnv->getName(), 'folder' => $db_folder]
+            '===> Downloading db backup of {site} to {dir}.',
+            ['site' => $liveEnv->getName(), 'dir' => $this->getLocalCopiesDbDir()]
+        );
+
+        $dbLocalFilename = sprintf(
+            '%s%s%s-db.tgz',
+            $this->getLocalCopiesDbDir(),
+            DIRECTORY_SEPARATOR,
+            $site->getName()
         );
         $this->request()->download(
             $lastBackup->getArchiveURL(),
-            $db_local_filename,
+            $dbLocalFilename,
             $options['overwrite']
         );
-        $this->logger->notice('DB Backup Downloaded to: {path}', ['path' => $db_local_filename]);
+        $this->logger->notice('Database backup downloaded into: {path}', ['path' => $dbLocalFilename]);
 
-        return $db_local_filename;
+        return $dbLocalFilename;
     }
 }
