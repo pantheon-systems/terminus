7bdc402b41e5d8bfcf73eeca08443d8b22810379
Sergei Churilo <sergei.churilo@pantheon.io>
2021-09-21 13:41:25 +0300

    Fix minor issues in CreateCommand

--- src/Commands/Site/CreateCommand.php
+++ src/Commands/Site/CreateCommand.php
@@ -6,7 +6,8 @@ use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
 use Pantheon\Terminus\Exceptions\TerminusException;
 
 /**
- * Class CreateCommand
+ * Class CreateCommand.
+ *
  * @package Pantheon\Terminus\Commands\Site
  */
 class CreateCommand extends SiteCommand
@@ -29,6 +30,8 @@ class CreateCommand extends SiteCommand
      *
      * @usage <site> <label> <upstream> Creates a new site named <site>, human-readably labeled <label>, using code from <upstream>.
      * @usage <site> <label> <upstream> --org=<org> Creates a new site named <site>, human-readably labeled <label>, using code from <upstream>, associated with <organization>.
+     *
+     * @throws TerminusException
      */
 
     public function create($site_name, $label, $upstream_id, $options = ['org' => null, 'region' => null,])
@@ -43,29 +46,28 @@ class CreateCommand extends SiteCommand
         ];
         // If the user specified a region, then include it in the workflow
         // options. We'll allow the API to decide whether the region is valid.
-        $region = isset($options['region']) ? $options['region'] : $this->config->get('command_site_options_region');
+        $region = $options['region'] ?? $this->config->get('command_site_options_region');
         if ($region) {
             $workflow_options['preferred_zone'] = $region;
         }
 
         $user = $this->session()->getUser();
 
-        // Locate upstream
+        // Locate upstream.
         $upstream = $user->getUpstreams()->get($upstream_id);
 
-        // Locate organization
+        // Locate organization.
         if (!is_null($org_id = $options['org'])) {
             $org = $user->getOrganizationMemberships()->get($org_id)->getOrganization();
             $workflow_options['organization_id'] = $org->id;
         }
 
-
-        // Create the site
+        // Create the site.
         $this->log()->notice('Creating a new site...');
         $workflow = $this->sites()->create($workflow_options);
         $this->processWorkflow($workflow);
 
-        // Deploy the upstream
+        // Deploy the upstream.
         if ($site = $this->getSite($workflow->get('waiting_for_task')->site_id)) {
             $this->log()->notice('Deploying CMS...');
             $this->processWorkflow($site->deployProduct($upstream->id));
