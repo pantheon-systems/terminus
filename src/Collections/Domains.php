f8f1ee96f82ef1f7b6196f01e6469cc1fe96f437
Sergei Churilo <sergei.churilo@pantheon.io>
2021-09-30 16:22:31 +0300

    Fix Domains collection

--- src/Collections/Domains.php
+++ src/Collections/Domains.php
@@ -3,12 +3,12 @@
 namespace Pantheon\Terminus\Collections;
 
 use Pantheon\Terminus\Exceptions\TerminusException;
-use Pantheon\Terminus\Exceptions\TerminusNotFoundException;
 use Pantheon\Terminus\Models\Domain;
 use Pantheon\Terminus\Request\RequestOperationResult;
 
 /**
- * Class Domains
+ * Class Domains.
+ *
  * @package Pantheon\Terminus\Collections
  */
 class Domains extends EnvironmentOwnedCollection
@@ -24,10 +24,15 @@ class Domains extends EnvironmentOwnedCollection
     protected $url = 'sites/{site_id}/environments/{environment_id}/domains';
 
     /**
-     * Adds a domain to the environment
+     * Adds a domain to the environment.
      *
-     * @param string $domain Domain to add to environment
-     * @return array
+     * @param string $domain
+     *   Domain to add to environment.
+     *
+     * @return \Pantheon\Terminus\Request\RequestOperationResult
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
      */
     public function create($domain): RequestOperationResult
     {
@@ -35,28 +40,27 @@ class Domains extends EnvironmentOwnedCollection
         $result = $this->request->request($url, ['method' => 'put']);
         if ($result->isError()) {
             throw new TerminusException(
-                'Error trying to add {domain} to {env}: {error}',
+                'Error trying to add {domain} to {site}.{env}: {error}',
                 [
                     'domain' => $domain,
-                    'site' => $this->getEnvironment()->getSite(),
-                    'env' => $this->getEnvironment(),
-                    'error' => $result->getStatusCodeReason(),
+                    'site' => $this->getEnvironment()->getSite()->getName(),
+                    'env' => $this->getEnvironment()->getName(),
+                    'error' => $result->getData(),
                 ]
             );
         }
+
         return $result;
     }
 
     /**
-     * Fetches domain data hydrated with recommendations
-     *
-     * @param array $options Additional information for the request
+     * Fetches domain data hydrated with recommendations.
      *
      * @return \Pantheon\Terminus\Collections\TerminusCollection
      */
-    public function fetchWithRecommendations($options = [])
+    public function fetchWithRecommendations()
     {
-        $this->setFetchArgs(['query' => ['hydrate' => ['as_list', 'recommendations',],],]);
+        $this->setFetchArgs(['query' => ['hydrate' => ['as_list', 'recommendations']]]);
 
         return $this->fetch();
     }
