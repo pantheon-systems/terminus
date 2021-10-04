29ac489ad814c343b2f7f7aa44d41851e726b478
Sergei Churilo <sergei.churilo@pantheon.io>
2021-10-04 14:10:46 +0300

    Refactor local:clone command
    * update to better error handling
    * add "site_dir" and "override" options
    * fix code style issues
    * remove redundant code

--- src/Commands/Local/CloneCommand.php
+++ src/Commands/Local/CloneCommand.php
@@ -2,22 +2,17 @@
 
 namespace Pantheon\Terminus\Commands\Local;
 
-use Consolidation\OutputFormatters\StructuredData\AbstractStructuredList;
-use Consolidation\OutputFormatters\StructuredData\PropertyList;
 use Pantheon\Terminus\Commands\TerminusCommand;
-use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
 use Pantheon\Terminus\Config\ConfigAwareTrait;
-use Pantheon\Terminus\Friends\LocalCopiesTrait;
+use Pantheon\Terminus\Exceptions\TerminusException;
 use Pantheon\Terminus\Helpers\Traits\CommandExecutorTrait;
-use Pantheon\Terminus\Models\Site;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
-use Pantheon\Terminus\Exceptions\TerminusException;
 use Robo\Contract\ConfigAwareInterface;
 
 /**
- * @name LocalCommands
- * Class CloneCommand
+ * Class CloneCommand.
+ *
  * @package Pantheon\Terminus\Commands\Local
  */
 class CloneCommand extends TerminusCommand implements SiteAwareInterface, ConfigAwareInterface
@@ -27,51 +22,51 @@ class CloneCommand extends TerminusCommand implements SiteAwareInterface, Config
     use CommandExecutorTrait;
 
     /**
-     *  CLone a copy of the site code into $HOME/pantheon-local-copies
+     * CLone a copy of the site code into $HOME/pantheon-local-copies
      *
      * @authorize
      *
      * @command local:clone
      * @aliases lc
      *
-     * @param string $site Site
+     * @param string $site_id The name or UUID of the site
+     * @param array $options
      *
-     * @usage <site> Clone's a local copy into "$HOME/pantheon-local-copies"
      * @return string
+     *
      * @throws \Pantheon\Terminus\Exceptions\TerminusException
      *
+     * @option site_dir Custom directory for the local copy. By default, the site name is used
+     * @option override Override the local copy if exists
+     *
+     * @usage <site> Clone's a local copy into "$HOME/pantheon-local-copies"
      */
-    public function clone($site) : string
+    public function clone(string $site_id, array $options = ['site_dir' => null, 'override' => null]): string
     {
-        $siteData = $site;
+        $site = $this->getSite($site_id);
+        $env = $site->getEnvironments()->get('dev');
 
-        if (!$siteData instanceof Site) {
-            $siteData = $this->getSite($site);
-            if (!$siteData instanceof Site) {
-                throw new TerminusException(
-                    "Cannot find site with the ID: {site}",
-                    ["site" => $site]
+        $localCopyDir = $site->getLocalCopyDir($options['site_dir'] ?? null);
+        $gitUrl = $env->connectionInfo()['git_url'] ?? null;
+        if (null === $gitUrl) {
+            throw new TerminusException('Failed to get connection Git URL');
+        }
+
+        $override = $options['override'] ?? false;
+        if (is_dir($localCopyDir . DIRECTORY_SEPARATOR . '.git')) {
+            if (!$override) {
+                $this->logger->notice(
+                    sprintf('The local copy of the site %s already exists in %s', $site->getName(), $localCopyDir)
                 );
+
+                return $localCopyDir;
             }
+
+            $this->execute('rm -rf %s', [$localCopyDir]);
         }
 
-        $env = $siteData->getEnvironments()->get('dev');
+        $this->execute('git clone %s %s', [$gitUrl, $localCopyDir]);
 
-        $clone_path = $siteData->getLocalCopyDir();
-        $connection =  $env->connectionInfo();
-
-        if (!is_dir($clone_path . DIRECTORY_SEPARATOR . ".git")) {
-            $this->execute(
-                "git clone %s %s",
-                [$connection['git_url'], $clone_path]
-            );
-        }
-        if (!is_dir($clone_path . DIRECTORY_SEPARATOR . ".git")) {
-            throw new TerminusException(
-                "Clone from Pantheon's Development repository failed.",
-                ['folder' => $clone_path]
-            );
-        }
-        return $clone_path;
+        return $localCopyDir;
     }
 }
