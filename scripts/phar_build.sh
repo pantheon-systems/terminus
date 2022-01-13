5b43c24e63168725c4e80521a7a3e89628898644
Sergei Churilo <sergei.churilo@pantheon.io>
2022-01-13 15:27:00 +0300

    Use "-n" instead of "! -z" in if statement

--- scripts/phar_build.sh
+++ scripts/phar_build.sh
@@ -9,7 +9,7 @@ echo "Building terminus.phar..."
 box compile
 echo "terminus.phar file has been created successfully!"
 
-if [[ ! -z "${TERMINUS_ON_PHAR_COMPLETE_REINSTALL_COMPOSER_WITH_DEV}" ]]; then
+if [[ -n "${TERMINUS_ON_PHAR_COMPLETE_REINSTALL_COMPOSER_WITH_DEV}" ]]; then
     echo "Reinstalling composer dependencies with --dev..."
     composer install --dev
 fi
