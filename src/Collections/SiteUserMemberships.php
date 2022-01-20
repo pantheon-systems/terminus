cbda050c28ce475a2f3d91b8ceb84a99dafac67f
Sergei Churilo <sergei.churilo@pantheon.io>
2022-01-20 15:10:38 +0300

    Use constants for site user membership roles

--- src/Collections/SiteUserMemberships.php
+++ src/Collections/SiteUserMemberships.php
@@ -5,7 +5,8 @@ namespace Pantheon\Terminus\Collections;
 use Pantheon\Terminus\Models\SiteUserMembership;
 
 /**
- * Class SiteUserMemberships
+ * Class SiteUserMemberships.
+ *
  * @package Pantheon\Terminus\Collections
  */
 class SiteUserMemberships extends SiteOwnedCollection
@@ -14,27 +15,39 @@ class SiteUserMemberships extends SiteOwnedCollection
      * @var string
      */
     protected $collected_class = SiteUserMembership::class;
+
     /**
      * @var boolean
      */
     protected $paged = true;
+
     /**
      * @var string
      */
     protected $url = 'sites/{site_id}/memberships/users';
 
     /**
-     * Adds this user as a member to the site
+     * Adds this user as a member to the site.
      *
-     * @param string $email Email of team member to add
-     * @param string $role  Role to assign to the new user
-     * @return Workflow
-     **/
+     * @param string $email
+     *   Email of team member to add.
+     * @param string $role
+     *   Role to assign to the new user.
+     *
+     * @return \Pantheon\Terminus\Models\Workflow
+     *
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
+     */
     public function create($email, $role)
     {
         return $this->getSite()->getWorkflows()->create(
             'add_site_user_membership',
-            ['params' => ['user_email' => $email, 'role' => $role,],]
+            [
+                'params' => [
+                    'user_email' => $email,
+                    'role' => $role,
+                ],
+            ]
         );
     }
 }
--- src/Commands/Site/Team/AddCommand.php
+++ src/Commands/Site/Team/AddCommand.php
@@ -4,11 +4,13 @@ namespace Pantheon\Terminus\Commands\Site\Team;
 
 use Pantheon\Terminus\Commands\TerminusCommand;
 use Pantheon\Terminus\Commands\WorkflowProcessingTrait;
+use Pantheon\Terminus\Models\SiteUserMembership;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
 
 /**
- * Class AddCommand
+ * Class AddCommand.
+ *
  * @package Pantheon\Terminus\Commands\Site\Team
  */
 class AddCommand extends TerminusCommand implements SiteAwareInterface
@@ -30,19 +32,23 @@ class AddCommand extends TerminusCommand implements SiteAwareInterface
      *
      * @usage <site> <user> Adds <user> as a team_member to <site>'s team.
      * @usage <site> <user> <role> Adds <user> as a <role> to <site>'s team.
+     *
+     * @throws \Pantheon\Terminus\Exceptions\TerminusException
+     * @throws \Exception
      */
-    public function add($site_id, $member, $role = 'team_member')
+    public function add($site_id, $member, $role = SiteUserMembership::ROLE_TEAM_MEMBER)
     {
         $site = $this->getSite($site_id);
         $team = $site->getUserMemberships();
 
-        if (($role !== 'team_member') && !(boolean)$site->getFeature('change_management')) {
-            $role = 'team_member';
+        if ($role !== SiteUserMembership::ROLE_TEAM_MEMBER && !$site->getFeature('change_management')) {
+            $role = SiteUserMembership::ROLE_TEAM_MEMBER;
             $this->log()->warning(
                 'Site does not have change management enabled, defaulting to user role {role}.',
                 compact('role')
             );
         }
+
         $workflow = $team->create($member, $role);
         $this->processWorkflow($workflow);
         $this->log()->notice($workflow->getMessage());
