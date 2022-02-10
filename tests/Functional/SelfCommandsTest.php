f20f1ec245ff21a6feed7c0d7da6ea3a1dec0787
Kevin Porras <kevin.porras@pantheon.io>
2022-02-10 12:22:01 -0600

    Add new test for self-update command. (#2333)
    * Add new test for self-update command.
    
    * Fix coding standards.
    
    * Update self-update library.

--- tests/Functional/SelfCommandsTest.php
+++ tests/Functional/SelfCommandsTest.php
@@ -20,5 +20,21 @@ class SelfCommandsTest extends TerminusTestBase
     public function testSelfUpdateCommand()
     {
         $this->assertCommandExists(self::SELF_UPDATE_COMMAND);
+
+        // Test that the command works when plugins are not installed.
+        $output = $this->terminus(self::SELF_UPDATE_COMMAND);
+        $this->assertEquals('No update available', $output);
+
+        // Test that the command works when plugins are installed.
+        $pluginList = $this->terminusWithStderrRedirected(
+            'self:plugin:install pantheon-systems/terminus-plugin-example'
+        );
+        $this->assertStringContainsString(
+            'Installed pantheon-systems/terminus-plugin-example',
+            $pluginList,
+            'Failed installing plugins to setup self:update command test.'
+        );
+        $output = $this->terminus(self::SELF_UPDATE_COMMAND);
+        $this->assertEquals('No update available', $output);
     }
 }
