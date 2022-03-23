dd4a7c638c096dd397a0913feca4b61b25191356
Kevin Porras <kevin.porras@pantheon.io>
2022-03-23 17:12:38 -0600

    Alter framework value for drupal and format table.

--- src/Commands/Site/InfoCommand.php
+++ src/Commands/Site/InfoCommand.php
@@ -4,6 +4,7 @@ namespace Pantheon\Terminus\Commands\Site;
 
 use Consolidation\OutputFormatters\StructuredData\PropertyList;
 use Pantheon\Terminus\Commands\StructuredListTrait;
+use Pantheon\Terminus\Models\TerminusModel;
 
 /**
  * Class InfoCommand
@@ -45,5 +46,38 @@ class InfoCommand extends SiteCommand
     public function info($site)
     {
         return $this->getPropertyList($this->sites->get($site));
+    }
+
+    /**
+     * @param TerminusModel $model A model with data to extract
+     * @return PropertyList A PropertyList-type object with applied filters
+     */
+    public function getPropertyList(TerminusModel $model)
+    {
+        $data = $model->serialize();
+        if ($this->input()->getOption('format') === 'table') {
+            // Manipulate framework to make it more user-friendly.
+            if (isset($data['framework'])) {
+                $data['framework'] = $this->getFrameworkFriendlyName($data['framework']);
+            }
+        }
+        $list = new PropertyList($data);
+        $list = $this->addBooleanRenderer($list);
+        $list = $this->addDatetimeRenderer($list, $model::$date_attributes);
+        return $list;
+    }
+
+    /**
+     * Get a user friendly framework name.
+     */
+    protected function getFrameworkFriendlyName(string $framework): string {
+        switch ($framework) {
+            case 'drupal':
+                return 'Drupal 6 or 7';
+            case 'drupal8':
+                return 'Drupal 8 or later';
+            default:
+                return $framework;
+        }
     }
 }
