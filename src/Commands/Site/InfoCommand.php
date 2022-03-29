b0648345338e00fec005f8b1677871352582a582
Kevin Porras <kevin.porras@pantheon.io>
2022-03-28 19:16:34 -0600

    [CMS-618] Alter framework value for drupal and format table. (#2351)
    * Alter framework value for drupal and format table.
    
    * Fix coding standards.
    
    * Use renderer function approach.
    
    * Add test for new framework handling.
    
    * Fix test for new framework formatter.

--- src/Commands/Site/InfoCommand.php
+++ src/Commands/Site/InfoCommand.php
@@ -4,6 +4,8 @@ namespace Pantheon\Terminus\Commands\Site;
 
 use Consolidation\OutputFormatters\StructuredData\PropertyList;
 use Pantheon\Terminus\Commands\StructuredListTrait;
+use Pantheon\Terminus\Models\TerminusModel;
+use Consolidation\OutputFormatters\StructuredData\AbstractStructuredList;
 
 /**
  * Class InfoCommand
@@ -45,5 +47,44 @@ class InfoCommand extends SiteCommand
     public function info($site)
     {
         return $this->getPropertyList($this->sites->get($site));
+    }
+
+    /**
+     * @param TerminusModel $model A model with data to extract
+     * @return PropertyList A PropertyList-type object with applied filters
+     */
+    public function getPropertyList(TerminusModel $model)
+    {
+        $list = new PropertyList($model->serialize());
+        $list = $this->addBooleanRenderer($list);
+        $list = $this->addDatetimeRenderer($list, $model::$date_attributes);
+        $list = $this->addFrameworkRenderer($list);
+        return $list;
+    }
+
+    /**
+     * Adds a renderer function to the structured list to format framework when rendering
+     *
+     * @param AbstractStructuredList $table
+     * @return RowsOfFields
+     */
+    private function addFrameworkRenderer(AbstractStructuredList $list)
+    {
+        $list->addRendererFunction(
+            function ($key, $cell_data) {
+                if (!is_numeric($key) && $key === 'framework') {
+                    switch ($cell_data) {
+                        case 'drupal':
+                            return 'Drupal 6 or 7';
+                        case 'drupal8':
+                            return 'Drupal 8 or later';
+                        default:
+                            return $cell_data;
+                    }
+                }
+                return $cell_data;
+            }
+        );
+        return $list;
     }
 }
