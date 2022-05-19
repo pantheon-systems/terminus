c7b23ae97d671fd7f34e6ad354a793920006c28a
Kevin Porras <kevin.porras@pantheon.io>
2022-05-19 15:51:59 -0600

    Delete now unused function.

--- src/Commands/Self/Plugin/PluginBaseCommand.php
+++ src/Commands/Self/Plugin/PluginBaseCommand.php
@@ -364,20 +364,6 @@ abstract class PluginBaseCommand extends TerminusCommand
     }
 
     /**
-     * Create dir with random name.
-     */
-    protected function createRandomDir($prefix = 'terminus', $dir = false)
-    {
-        $fs = $this->getLocalMachine()->getFileSystem();
-        $tempfile = $fs->tempnam($dir ? $dir : sys_get_temp_dir(), $prefix ? $prefix : '');
-        if ($fs->exists($tempfile)) {
-            $fs->remove($tempfile);
-        }
-        $fs->mkdir($tempfile, 0700);
-        return $tempfile;
-    }
-
-    /**
      * Register our shutdown function if it hasn't already been registered.
      */
     protected function registerCleanupFunction($path)
