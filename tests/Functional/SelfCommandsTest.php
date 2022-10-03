665a9dfa20f54270db96a4cb6f17ceef2f88ddc2
Sergei Churilo <sergei.churilo@pantheon.io>
2022-10-03 10:08:11 +0300

    debug tests

--- tests/Functional/SelfCommandsTest.php
+++ tests/Functional/SelfCommandsTest.php
@@ -16,16 +16,25 @@ class SelfCommandsTest extends TerminusTestBase
      *
      * @group self
      * @group long
+     * @group debug
      */
     public function testSelfUpdateCommand()
     {
         $this->assertCommandExists(self::SELF_UPDATE_COMMAND);
 
         // Test that the command works when plugins are not installed.
-        $output = $this->terminus(self::SELF_UPDATE_COMMAND);
+        [$output, $exitCode, $error] = static::callTerminus(self::SELF_UPDATE_COMMAND);
+        if (0 !== $exitCode
+            && false !== strpos($error, 'rate limit exceeded')
+        ) {
+            // @todo: fix CMS-972
+            $this->markTestSkipped(sprintf('Failed executing %s command: %s', self::SELF_UPDATE_COMMAND, $error));
+        }
+
         $this->assertEquals('No update available', $output);
 
         // Test that the command works when plugins are installed.
+        $this->terminus('self:plugin:uninstall pantheon-systems/terminus-plugin-example', [], false);
         $pluginList = $this->terminusWithStderrRedirected(
             'self:plugin:install pantheon-systems/terminus-plugin-example'
         );
