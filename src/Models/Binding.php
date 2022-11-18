e7bc3ba202d68cddbafacbde9c3acb2e4ee69ca4
Sachin Jeeragal <sachinjeeragal@HPS464s-MacBook-Pro.local>
2022-11-18 10:46:52 +0530

    binding.php

--- src/Models/Binding.php
+++ src/Models/Binding.php
@@ -19,6 +19,6 @@ class Binding extends TerminusModel
     public function getUsername()
     {
         $database_runtime = $this->get('database_runtime');
-        return $this->has('legacy_username') ? $this->get('legacy_username') : $this->$database_runtime->username;
+        return $this->has('legacy_username') ? $this->get('legacy_username') : $this->$database_runtime->get('username');
     }
 }
