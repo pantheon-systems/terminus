b80075b16041d1af109b46ca818ee3ab1dbcf7ef
Kevin Porras <kevin.porras@pantheon.io>
2022-12-20 09:50:18 -0600

    Cleanup command.

--- src/Commands/Env/SyncCodeCommand.php
+++ src/Commands/Env/SyncCodeCommand.php
@@ -19,25 +19,18 @@ class SyncCodeCommand extends TerminusCommand implements SiteAwareInterface
     use WorkflowProcessingTrait;
 
     /**
-     * Deploys code to the Test or Live environment.
-     * Notes:
-     *   - Deploying the Test environment will deploy code from the Dev environment.
-     *   - Deploying the Live environment will deploy code from the Test environment.
+     * Sync code for the given environment (only dev and multidev allowed).
      *
      * @authorize
      *
      * @command env:sync-code
      * @aliases sync-code
      *
-     * @param string $site_env Site & environment in the format `site-name.env` (only Test or Live environment)
+     * @param string $site_env Site & environment in the format `site-name.env` (only Dev or Multidev)
      *
      * @throws \Pantheon\Terminus\Exceptions\TerminusException
      *
-     * @usage <site>.test Deploy code from <site>'s Dev environment to the Test environment.
-     * @usage <site>.live Deploy code from <site>'s Test environment to the Live environment.
-     * @usage <site>.test --sync-content Deploy code from <site>'s Dev environment to the Test environment and clone content from the Live environment to the Test environment.
-     * @usage <site>.live --updatedb Deploy code from <site>'s Test environment to the Live environment and run Drupal's update.php.
-     * @usage <site>.live --note=<message> Deploy code from <site>'s Test environment to the Live environment with the deploy log message <message>.
+     * @usage <site>.<env> Sync code into the <site>'s Dev or multidev environment.
      */
     public function deploy(
         $site_env
@@ -45,6 +38,10 @@ class SyncCodeCommand extends TerminusCommand implements SiteAwareInterface
         $this->requireSiteIsNotFrozen($site_env);
         $site = $this->getSite($site_env);
         $env = $this->getEnv($site_env);
+
+        if ($env->getName() === 'test' || $env->getName() === 'live') {
+            throw new TerminusException('Test and live are not valid envs for this command.');
+        }
 
         $params = [
             'converge' => true,
