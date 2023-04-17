394dac4e069dacf5e62306962322cbf0addd448e
Tom Stovall <stovak@gmail.com>
2023-04-17 16:39:51 -0700

    if not in the context of a github action, lable the build as a development build

--- scripts/phar_build.sh
+++ scripts/phar_build.sh
@@ -5,6 +5,14 @@ set -e
 echo "Installing composer dependencies with --no-dev..."
 composer install --no-dev
 
+VERSION=$(cat .version)
+## if not in the context of a github action, lable the build as
+## a dev build
+if [ -n "${GITHUB_RUN_ID}" ]
+then
+    echo "-dev" >> .version
+fi
+
 echo "Building terminus.phar..."
 box compile
 echo "terminus.phar file has been created successfully!"
@@ -13,3 +21,6 @@ if [[ -n "${TERMINUS_ON_PHAR_COMPLETE_REINSTALL_COMPOSER_WITH_DEV}" ]]; then
     echo "Reinstalling composer dependencies with --dev..."
     composer install --dev
 fi
+
+## restore version to correct value
+echo ${VERSION} > .version
