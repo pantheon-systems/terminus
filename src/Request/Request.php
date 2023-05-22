200280b6d7fbdfef313bcfd7836ea0153c2abebd
Sandy Vanderbleek <svanderbleek@Sandys-MBP.attlocal.net>
2023-05-22 04:22:02 -0400

    Headers of command and trace metadata

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -66,6 +66,8 @@ class Request implements
     . "Data: {data}\n"
     . "Status Code: {status_code}";
 
+    const SENSITIVE_COMMANDS = ["auth:login", "lock:enable",];
+
     protected ClientInterface $client;
 
     /**
@@ -423,6 +425,8 @@ class Request implements
         return [
             'User-Agent' => $this->userAgent(),
             'Accept' => 'application/json',
+            'X-Pantheon-Trace-Id' => $GLOBALS['PANTHEON_TRACE_ID'],
+            'X-Pantheon-Terminus-Command' => $this->terminusCommand(),
         ];
     }
 
@@ -440,6 +444,22 @@ class Request implements
             $config->get('php_version'),
             $config->get('script')
         );
+    }
+
+    /**
+     * Gives the terminus command.
+     *
+     * @return string
+     */
+    private function terminusCommand()
+    {
+        $argv = $GLOBALS['argv'];
+        $command = $argv[1];
+        if (in_array($command, self::SENSITIVE_COMMANDS)) {
+           return $command;
+        } else {
+            return implode(" ", array_slice($argv, 1));
+        }
     }
 
     /**
