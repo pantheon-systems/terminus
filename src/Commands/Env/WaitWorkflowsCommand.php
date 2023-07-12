7ac82dcb8400d3b881f0a97ba5b8ea15cb88043b
Kevin Porras <kevin.porras@pantheon.io>
2023-07-12 15:06:24 -0600

    Wait for workflows to finish.

--- src/Commands/Env/WaitWorkflowsCommand.php
+++ src/Commands/Env/WaitWorkflowsCommand.php
@@ -26,6 +26,7 @@ class WaitWorkflowsCommand extends TerminusCommand implements SiteAwareInterface
      *
      * @param string $site_env Site & environment in the format `site-name.env`
      * @option bool $healthcheck Whether to wait for the healthcheck to pass
+     * @option int $timeout How long to wait for the workflows to finish
      *
      * @throws TerminusException
      *
@@ -33,15 +34,53 @@ class WaitWorkflowsCommand extends TerminusCommand implements SiteAwareInterface
      */
     public function waitWorkflows($site_env, $options = [
         'healthcheck' => true,
+        'timeout' => 300,
     ])
     {
         $this->requireSiteIsNotFrozen($site_env);
+        $site = $this->getSite($site_env);
         $env = $this->getEnv($site_env);
 
-        if ($options['healthcheck']) {
-            $env->wake();            
+        $workflows = $site->getWorkflows()->setPaging(false)->fetch()->allByEnvironmentIdAndStatus($env->id, 'unfinished');
+        $this->log()->notice('Waiting for {number} workflow(s).', ['number' => count($workflows)]);
+
+        $startWaiting = time();
+        foreach ($workflows as $workflow) {
+            $description = $workflow->get('description');
+            $workflow->fetch();
+            while ($workflow->isUnfinished()) {
+                $this->log()->notice(
+                    "Workflow '{description}' is running...",
+                    ['description' => $description]
+                );
+
+                if (time() - $startWaiting > $options['timeout']) {
+                    throw new TerminusException(
+                        'Timeout waiting for workflows to finish. Please check the status of the workflows manually.'
+                    );
+                }
+                sleep(5);
+                $workflow->fetch();
+            }
+            // Abort if workflow failed.
+            if ($workflow->isFailed()) {
+                throw new TerminusException(
+                    'Workflow failed. Please check the status of the workflows manually.'
+                );
+            }
+            if ($workflow->isSuccessful()) {
+                $this->log()->notice(
+                    "Workflow '{description}' succeeded.",
+                    ['description' => $description]
+                );
+            }
         }
 
-        $this->log()->notice('OK');
+        if ($options['healthcheck']) {
+            $this->log()->notice('Running healthcheck on environment...');
+            $env->wake();
+        }
+
+        $this->log()->notice('All done!');
     }
 }
