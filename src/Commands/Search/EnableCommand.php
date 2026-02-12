944771879c831902c430a28ab41fedfccf9b2000
Scott Buscemi <scott.buscemi@pantheon.io>
2026-02-12 11:38:12 -0800

    revert a change to elasticsearch capitalization

--- src/Commands/Search/EnableCommand.php
+++ src/Commands/Search/EnableCommand.php
@@ -56,21 +56,14 @@ class EnableCommand extends TerminusCommand implements SiteAwareInterface
         }
 
         $this->processWorkflow($workflow);
+        $this->log()->notice($workflow->getMessage());
 
-        // Display custom success message with proper capitalization
-        if ($workflow->isSuccessful()) {
-            if ($flavor === 'elasticsearch') {
-                $this->log()->notice('Enabled Elasticsearch for {site}.', ['site' => $site_id]);
-                $this->log()->notice(
-                    'Read the documentation to complete the Elasticsearch configuration for your site: ' .
-                    'https://docs.pantheon.io/pantheon-search'
-                );
-            } else {
-                $this->log()->notice('Enabled Solr for {site}.', ['site' => $site_id]);
-            }
-        } else {
-            // If workflow failed, show the API error message
-            $this->log()->notice($workflow->getMessage());
+        // Provide documentation link for successful Elasticsearch enablement
+        if ($flavor === 'elasticsearch' && $workflow->isSuccessful()) {
+            $this->log()->notice(
+                'Read the documentation to complete the Elasticsearch configuration for your site: ' .
+                'https://docs.pantheon.io/pantheon-search'
+            );
         }
     }
 
