b9834d922326098ea1dd3d2f1552bcbbc0a59107
Edmund Turbin <785171+spicecadet@users.noreply.github.com>
2026-03-09 16:11:02 -0400

    Added functional tests and updated readme

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ## Status
 
 [![Commit Build](https://github.com/pantheon-systems/terminus/actions/workflows/ci.yml/badge.svg?branch=v3.0)](https://github.com/pantheon-systems/terminus/actions/workflows/ci.yml)
-[![Actively Maintained](https://img.shields.io/badge/Pantheon-Actively_Maintained-yellow?logo=pantheon&color=FFDC28)](https://pantheon.io/docs/oss-support-levels#actively-maintained-support)
 
 ## About
 
@@ -22,9 +21,11 @@ Our documentation is kept in the Terminus Manual, located here: https://pantheon
 |------------------------|------------|
 | MacOS                  | 10.14+     |
 | Ubuntu                 | Latest LTS |
-| Windows + WSL + Ubuntu | 20.0+      |
+| Windows + WSL + Ubuntu | TBD        |
 
 ### Package Manager
+
+- [apt](https://ubuntu.com/server/docs/package-management) for Ubuntu/WinWSL-Ubuntu
 
 - [Homebrew](https://brew.sh) for mac
 
@@ -34,11 +35,9 @@ These packages are required to take full advantage of Terminus.
 
 - [Composer 2](https://getcomposer.org) (Needed for the plugin manager component)
 
-- [PHP](https://www.php.net) (v8.2+)
+- [PHP](https://www.php.net) (v7.4+)
 
 - [Git](https://help.github.com/articles/set-up-git/) (May be needed for the plugin manager component)
-
-- OpenSSH 7.8 or later
 
 #### Recommended Packages
 
@@ -57,20 +56,10 @@ Terminus is published as a package under pantheon-systems/external. To install i
 brew install pantheon-systems/external/terminus
 ```
 
-### Standalone Terminus PHAR
+### Ubuntu / WinWSL+Ubuntu:
 
-The commands below will:
+`*** TBD ***`
 
-- Create a `terminus` folder in your home directory (`~/`)
-- Get the latest release tag of Terminus
-- Download and save the release as `~/terminus/terminus`
-- Make the file executable
-- Add a symlink to your local `bin` directory for the Terminus executable
+### Other installation methods
 
-```bash
-mkdir -p ~/terminus && cd ~/terminus
-curl -L https://github.com/pantheon-systems/terminus/releases/download/3.6.1/terminus.phar --output terminus
-chmod +x terminus
-./terminus self:update
-sudo ln -s ~/terminus/terminus /usr/local/bin/terminus
-```
+Refer to the [Terminus manual](https://pantheon.io/docs/terminus/install#install-terminus) for other installation methods.
--- tests/Functional/RedisCommandsTest.php
+++ tests/Functional/RedisCommandsTest.php
@@ -34,4 +34,161 @@ class RedisCommandsTest extends TerminusTestBase
     {
         $this->assertTerminusCommandSucceedsInAttempts(sprintf('redis:disable %s', $this->getSiteName()));
     }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfo()
+    {
+        $output = $this->terminus(sprintf('redis:info %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoMemory
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoMemory()
+    {
+        $output = $this->terminus(sprintf('redis:info:memory %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:memory command should return output');
+        $this->assertStringContainsString('used_memory', $output);
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoCpu
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoCpu()
+    {
+        $output = $this->terminus(sprintf('redis:info:cpu %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:cpu command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoStats
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoStats()
+    {
+        $output = $this->terminus(sprintf('redis:info:stats %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:stats command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoReplication
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoReplication()
+    {
+        $output = $this->terminus(sprintf('redis:info:replication %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:replication command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoClients
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoClients()
+    {
+        $output = $this->terminus(sprintf('redis:info:clients %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:clients command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoServer
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoServer()
+    {
+        $output = $this->terminus(sprintf('redis:info:server %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:server command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoPersistence
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoPersistence()
+    {
+        $output = $this->terminus(sprintf('redis:info:persistence %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:persistence command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoKeyspace
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoKeyspace()
+    {
+        $output = $this->terminus(sprintf('redis:info:keyspace %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:keyspace command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InfoCommand::infoCommandstats
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInfoCommandstats()
+    {
+        $output = $this->terminus(sprintf('redis:info:commandstats %s.dev', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis info:commandstats command should return output');
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\FlushdbCommand
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisFlushdb()
+    {
+        $this->assertTerminusCommandSucceedsInAttempts(sprintf('redis:flushdb %s.dev', $this->getSiteName()));
+    }
+
+    /**
+     * @test
+     * @covers \Pantheon\Terminus\Commands\Redis\InsightCommand
+     *
+     * @group redis
+     * @group long
+     */
+    public function testRedisInsightUrlOnly()
+    {
+        $output = $this->terminus(sprintf('redis:insight %s.dev --url-only', $this->getSiteName()));
+        $this->assertNotEmpty($output, 'Redis insight --url-only should return URL');
+        $this->assertStringContainsString('redis://', $output);
+    }
 }
