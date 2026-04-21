8a474fcc4c0c31d9d87f93d65053226412cd08b6
Sumit Madan <sumit.madan@pantheon.io>
2026-04-21 13:59:09 +0530

    fix(BUGS-11139): Only include wp_replace_siteurl if site is wordpress.

--- src/Models/Environment.php
+++ src/Models/Environment.php
@@ -173,13 +173,16 @@ class Environment extends TerminusModel implements
             'updatedb' => $options['updatedb'] ?? 0,
             'clear_cache' => $options['clear_cache'] ?? false,
         ];
-        if (!empty($options['from_url']) && !empty($options['to_url'])) {
-            $params['wp_replace_siteurl']['from_url'] = $options['from_url'];
-            $params['wp_replace_siteurl']['to_url'] = $options['to_url'];
-        } else {
-            // Automatically detect environment URLs for search-replace
-            $params['wp_replace_siteurl']['from_url'] = 'https://' . $from_env->domain();
-            $params['wp_replace_siteurl']['to_url'] = 'https://' . $this->domain();
+        // Only add wp_replace_siteurl for WordPress sites
+        if ($this->getSite()->getFramework()->isWordpressFramework()) {
+            if (!empty($options['from_url']) && !empty($options['to_url'])) {
+                $params['wp_replace_siteurl']['from_url'] = $options['from_url'];
+                $params['wp_replace_siteurl']['to_url'] = $options['to_url'];
+            } else {
+                // Automatically detect environment URLs for search-replace
+                $params['wp_replace_siteurl']['from_url'] = 'https://' . $from_env->domain();
+                $params['wp_replace_siteurl']['to_url'] = 'https://' . $this->domain();
+            }
         }
         return $this->getWorkflows()->create(
             'clone_database',
